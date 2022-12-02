87df17a76f02aa3e23fed11bd9a4d568da76aded
RosalynLP <rosalyn.pearson@phs.scot>
2022-12-02 07:40:18 +0000

    fix tooltip

--- shiny_app/indicators/vaccines/vaccines_functions.R
+++ shiny_app/indicators/vaccines/vaccines_functions.R
@@ -9,18 +9,22 @@ make_vaccine_wastage_plot <- function(data){
   xaxis_plots[["title"]] <- "Month"
 
   p <- plot_ly(data, x = ~Month, y = ~NumberOfDosesAdministered,
-               type = 'bar', text = ~PercentageWasted,
-               name = 'Number of doses administered',
+               type = 'bar',
+               text = ~paste0("<b>Month</b>: ", format(Month, "%b %y"), "\n",
+                              "<b>Doses administered</b>: ", format(NumberOfDosesAdministered, big.mark=","), "\n",
+                              "<b>Doses wasted</b>: ", format(NumberOfDosesWasted, big.mark=","), "\n",
+                              "<b>Percentage wasted</b>: ", PercentageWasted, "%\n"),
+               name = 'Doses administered',
                color = phs_colors("phs-teal"),
-               hovertemplate = paste0('<b>Month</b>: %{x}<br>',
-                                      '<b>Percentage wasted</b>: %{text}<br>'))%>%
+               hovertemplate = "%{text}")%>%
 
-    add_trace(y = ~NumberOfDosesWasted, name = 'Number of doses wasted',
+    add_trace(y = ~NumberOfDosesWasted, name = 'Doses wasted',
               color = phs_colors("phs-blue")) %>%
 
     layout(margin = list(b = 80, t = 5),
            yaxis = yaxis_plots, xaxis = xaxis_plots,
-           legend = list(x = 100, y = 0.5),barmode = 'stack') %>%
+           legend = list(x = 100, y = 0.5),
+           barmode = 'stack') %>%
 
     config(displaylogo = F, displayModeBar = TRUE,
            modeBarButtonsToRemove = bttn_remove )
