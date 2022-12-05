77b81d78e6afe7a56c0c63dbae200638bc12b2af
RosalynLP <rosalyn.pearson@phs.scot>
2022-12-05 15:11:40 +0000

    tidy wastewater

--- Dashboard Data Transfer/Transfer Scripts/transfer_wastewater.R
+++ Dashboard Data Transfer/Transfer Scripts/transfer_wastewater.R
@@ -3,9 +3,7 @@
 
 ##### Wastewater
 
-#i_wastewater <- read_excel(glue(input_data, "wastewater_{format(report_date -5,'%d%m%y')}.xlsx"))
-i_wastewater <- read_excel(glue("/conf/C19_Test_and_Protect/Test & Protect - Warehouse/Wastewater Analysis/Data/", "wastewater_{format(report_date -9,'%y%m%d')}.xlsx"))
-i_wastewater
+i_wastewater <- read_excel(glue(input_data, "wastewater_{format(report_date -5,'%d%m%y')}.xlsx"))
 
 g_wastewater <- i_wastewater %>%
   dplyr::rename(Date = Date7DayEnding,
