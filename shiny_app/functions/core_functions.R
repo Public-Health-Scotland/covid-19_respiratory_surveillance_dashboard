814a56d4052dc16a4ba6691b68839052f3ea6116
Calum Massie <calumm07@nssrstudio08.csa.scot.nhs.uk>
2022-12-05 12:24:38 +0000

    Adding reported cases chart and table

--- shiny_app/functions/core_functions.R
+++ shiny_app/functions/core_functions.R
@@ -171,3 +171,50 @@ convert_date_to_month <- function(date){
   return(date)
 }
 
+vline <- function(x, width=3.0, color="black", ...) {
+  l_shape = list(
+    type = "line",
+    y0 = 0, y1 = 1, yref = "paper", # i.e. y as a proportion of visible region
+    x0 = x, x1 = x,
+    line = list(color = color)
+  )
+  return(l_shape)
+}
+
+annotation <- function(frac, y, note, color){
+  ann <- list(yref = "paper",
+              xref = "paper",
+              y = y,
+              x = frac,
+              text = note,
+              # Styling annotations' text:
+              font = list(color = color,
+                          size = 14),
+              showarrow=FALSE)
+  return(ann)
+}
+
+add_vline <- function(p, x, ...){
+  l_shape <- vline(x, ...)
+  p %>% layout(shapes=list(l_shape))
+}
+
+
+add_lines_and_notes <- function(p, dataframe, ycol, xs, notes, colors){
+
+
+
+  for (i in seq(length(xs))){
+
+    p %<>% add_segments(x = xs[i], xend = xs[i],
+                        y = 0,
+                        yend = max(dataframe[[ycol]]),
+                        name = c(notes[i]),
+                        line = list(color = colors[i], width = 3)
+    )
+
+  }
+
+  return(p)
+}
+
--- shiny_app/indicators/vaccines/vaccines_ui.R
+++ shiny_app/indicators/vaccines/vaccines_ui.R
@@ -1,43 +1,43 @@
 
 tagList(
-  
-  tags$div(class = "headline", 
-           h3(glue("Figures from {Vaccine_Wastage %>% tail(1) %>% 
+
+  tags$div(class = "headline",
+           h3(glue("Figures from {Vaccine_Wastage %>% tail(1) %>%
                 .$Month %>% convert_opendata_date() %>% convert_date_to_month()}")),
-              valueBox(value = {Vaccine_Wastage %>% tail(1) %>% 
-               .$NumberOfDosesAdministered %>% 
-               format(big.mark = ",")}, 
-               subtitle = "Doses Administered",
+              valueBox(value = {Vaccine_Wastage %>% tail(1) %>%
+               .$NumberOfDosesAdministered %>%
+               format(big.mark = ",")},
+               subtitle = "Doses administered",
                color = "teal",
                icon = icon_no_warning_fn("syringe")),
-           valueBox(value = {Vaccine_Wastage %>% tail(1) %>% 
-               .$NumberOfDosesWasted %>% 
-               format(big.mark = ",")}, 
-               subtitle = "Doses Wasted",
+           valueBox(value = {Vaccine_Wastage %>% tail(1) %>%
+               .$NumberOfDosesWasted %>%
+               format(big.mark = ",")},
+               subtitle = "Doses wasted",
                color = "teal",
                icon = icon_no_warning_fn("dumpster")),
-           valueBox(value = {Vaccine_Wastage %>% tail(1) %>% 
-               .$PercentageWasted %>% 
-               paste0("%")}, 
+           valueBox(value = {Vaccine_Wastage %>% tail(1) %>%
+               .$PercentageWasted %>%
+               paste0("%")},
                subtitle = "Percentage",
                color = "teal",
                icon = icon_no_warning_fn("percent"))),
-  
+
   fluidRow(height = "50px", br()),
-  
-  tabBox(width = NULL, type = "pills", 
-         tabPanel("Plot", 
-                  tagList(h3("COVID-19 Vaccine Wastage"),
+
+  tabBox(width = NULL, type = "pills",
+         tabPanel("Plot",
+                  tagList(h3("COVID-19 vaccine wastage"),
                           plotlyOutput("vaccine_wastage_plot"))),
          tabPanel("Data",
-                  tagList(h3("COVID-19 Vaccine Wastage Data"),
+                  tagList(h3("COVID-19 vaccine wastage data"),
                           dataTableOutput("vaccine_wastage_table")))),
-  
-  fluidRow(column(width = 6, 
+
+  fluidRow(column(width = 6,
                   plotlyOutput("vaccine_wastage_reason_plot")),
            column(width = 6,
                   dataTableOutput("vaccine_wastage_reason_table")))
-  
+
 
 
 )
