0d78918d291ea1a8140817ce399bfefa4e4868fb
RosalynLP <rosalyn.pearson@phs.scot>
2022-12-06 08:15:42 +0000

    updating vertical lines fn
    removed unneeded functions
    commented add_lines_and_notes

--- shiny_app/functions/plot_functions.R
+++ shiny_app/functions/plot_functions.R
@@ -1,36 +1,12 @@
-# vline functions ----
-vline <- function(x, width=3.0, color="black", ...) {
-  l_shape = list(
-    type = "line",
-    y0 = 0, y1 = 1, yref = "paper", # i.e. y as a proportion of visible region
-    x0 = x, x1 = x,
-    line = list(color = color)
-  )
-  return(l_shape)
-}
 
-annotation <- function(frac, y, note, color){
-  ann <- list(yref = "paper",
-              xref = "paper",
-              y = y,
-              x = frac,
-              text = note,
-              # Styling annotations' text:
-              font = list(color = color,
-                          size = 14),
-              showarrow=FALSE)
-  return(ann)
-}
-
-add_vline <- function(p, x, ...){
-  l_shape <- vline(x, ...)
-  p %>% layout(shapes=list(l_shape))
-}
-
-
-add_lines_and_notes <- function(p, dataframe, ycol, xs, notes, colors){
-
-
+# Adds vertical lines with notes in the legend ----
+add_lines_and_notes <- function(p,           # plot to add lines to
+                                dataframe,   # dataframe the plot is based off
+                                ycol,        # colname of dataframe col which defines the y trace
+                                             #    to get ymin and ymax of vertical line from
+                                xs,          # vector of x locations for vertical lines
+                                notes,       # vector of notes for each vertical line
+                                colors){     # vector of colours for each line
 
   for (i in seq(length(xs))){
 
