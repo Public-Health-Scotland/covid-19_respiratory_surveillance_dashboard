a8fd1a7e89554dc2120689cb6d3bed3086a18d5c
Jennifer Evans <jennie04@nssrstudio06.csa.scot.nhs.uk>
2022-12-09 14:45:23 +0000

    adding collapse panels to metadata

---
+++ shiny_app/indicators/metadata/metadata_ui.R
@@ -0,0 +1,9 @@
+tagList(h1("Metadata"),
+  bsCollapse(id="notes_collapse", open = "Panel 1",
+                   bsCollapsePanel("Cases", "write something here"),
+                   bsCollapsePanel("Hospital admissions", "write more and more and more"),
+                   bsCollapsePanel("Hospital Occupancy", "write more and more"),
+                   bsCollapsePanel("Vaccine wastage", "write something else here")
+                   )
+
+)#tagList
