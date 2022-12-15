c638759def9d1b2891fcb8464a25d23bf5ef9623
Calum Massie <calumm07@nssrstudio09.csa.scot.nhs.uk>
2022-12-15 16:06:27 +0000

    Adding definition of CI to ons section of metadata

--- shiny_app/indicators/metadata/metadata_ui.R
+++ shiny_app/indicators/metadata/metadata_ui.R
@@ -18,6 +18,10 @@ tagList(h1("Metadata"),
                                             href="https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/bulletins/coronaviruscovid19infectionsurveypilot/previousReleases"),
                                             "."),
                                    p(strong("Source: The Office for National Statistics (ONS)")),
+                                   p("Please note, a",strong("confidence interval"), "gives an indication of the degree of uncertainty of an estimate, showing the precision of a sample estimate.
+                                     The 95% confidence intervals are calculated so that if we repeated the study many times, 95% of the time the true unknown value would lie between
+                                     the lower and upper confidence limits. A wider interval indicates more uncertainty in the estimate. Overlapping confidence intervals indicate that
+                                     there may not be a true difference between two estimates."),
                                    br(),
 
                                    # R number ----
