dc4ab43c3927c7ffc7b7baa3b94eb412d2cee31d
Mark Hamilton <markha06@nssrstudio08.csa.scot.nhs.uk>
2022-12-16 09:19:42 +0000

    Updated hospital admissions scripts

---
+++ shiny_app/indicators/hospital_admissions/hospital_admissions_functions.R
@@ -0,0 +1,315 @@
+
+###########################
+### HOSPITAL ADMISSIONS ###
+###########################
+
+# Daily Hospital Admissions plot
+make_hospital_admissions_plot <- function(data){
+  
+  # Wrangle Data
+  data <- data %>%
+    arrange(desc(AdmissionDate)) %>%
+    mutate(AdmissionDate = convert_opendata_date(AdmissionDate))
+  
+  # Provisional data
+  prov_data <- data %>%
+    filter(ProvisionalFlag == 1) %>%
+    select(AdmissionDate, TotalInfections, SevenDayAverage)
+  
+  # Remainder of the data
+  non_prov_data <- data %>%
+    filter(ProvisionalFlag == 0) %>%
+    select(AdmissionDate, TotalInfections, SevenDayAverage)
+  
+  # Create axis titles
+  yaxis_title <- "Number of admissions"
+  xaxis_title <- ""
+  
+  #Modifying standard layout
+  yaxis_plots[["title"]] <- yaxis_title
+  xaxis_plots[["title"]] <- xaxis_title
+  
+  #Text for tooltip
+  tooltip_trend <- c(paste0("Date: ", format(non_prov_data$AdmissionDate, "%d %b %y"),
+                            "<br>", "Admissions: ", non_prov_data$TotalInfections,
+                            "<br>", "7 Day Average: ", format(non_prov_data$SevenDayAverage, nsmall=0, digits=3)))
+  
+  # Text for tooltip (provisional data)
+  tooltip_trend_prov <- c(paste0("Provisional data: ",
+                                 "<br>", "Date: ", format(prov_data$AdmissionDate, "%d %b %y"),
+                                 "<br>", "Admissions: ", prov_data$TotalInfections,
+                                 "<br>", "7 Day Average: ", format(prov_data$SevenDayAverage, nsmall=0, digits=3)))
+  
+  #Creating time trend plot
+  p <- plot_ly(non_prov_data, x = ~AdmissionDate) %>%
+    add_lines(y = ~TotalInfections, line = list(color = '#0078D4', width=0.8),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Count") %>%
+    add_lines(y = ~SevenDayAverage, line = list(color = '#000000'),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "7 day average") %>%
+    #Layout
+    layout(margin = list(b = 80, t = 5), #to avoid labels getting cut out
+           yaxis = yaxis_plots, xaxis = xaxis_plots,
+           legend = list(x = 100, y = 0.5)) %>% #position of legend
+    # leaving only save plot button
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove )
+  
+  # Add in provisional data
+  p %<>% add_lines(data=prov_data, x=~AdmissionDate, y=~TotalInfections, line=list(color="#5c6164", width=0.8),
+                   text = tooltip_trend_prov, hoverinfo = "text",
+                   name = "Count (provisional)") %>%
+    add_lines(data=prov_data, y=~SevenDayAverage, line=list(color="#434343"),
+              text = tooltip_trend_prov, hoverinfo = "text",
+              name = "7 day average (provisional)")
+  
+  # Add in vertical lines
+  p %<>% add_segments(x = "2022-01-06", xend = "2022-01-06",
+               y = 0,
+               yend = max(data[["TotalInfections"]]),
+               name = c("From 5 Jan cases include PCR + LFD"),
+               line = list(color = phs_colours("phs-magenta"), width = 3)) %>%
+         add_segments(x = "2022-05-01", xend = "2022-05-01",
+               y = 0,
+               yend = max(data[["TotalInfections"]]),
+               name = c("Change in testing policy on 1 May"),
+               line = list(color = phs_colours("phs-teal"), width = 3))
+  
+  return(p)
+  
+}
+
+# Weekly Admissions by SIMD plot
+make_hospital_admissions_simd_plot <- function(data){
+  
+  data <- Admissions_SimdTrend
+  
+  data %<>%
+    arrange(desc(WeekEnding)) %>%
+    mutate(WeekEnding = convert_opendata_date(WeekEnding))
+  
+  yaxis_plots[["title"]] <- "Number of admissions"
+  xaxis_plots[["title"]] <- "Week ending"
+  
+  p <- plot_ly(data) %>%
+    add_trace(x = ~WeekEnding, y = ~NumberOfAdmissions, split = ~SIMD, text=~SIMD,
+              type="scatter", mode="lines",
+              color=~SIMD,
+              colors=phs_colours(c("phs-blue", "phs-liberty-30", "phs-liberty-30",
+                                   "phs-liberty-30", "phs-green")),
+              hovertemplate = paste0('<b>Week ending</b>: %{x}<br>',
+                                     '<b>SIMD quintile</b>: %{text}<br>',
+                                     '<b>Number of admissions</b>: %{y}')
+    ) %>%
+    layout(margin = list(b = 80, t = 5),
+           yaxis = yaxis_plots, xaxis = xaxis_plots,
+           legend = list(x = 100, y = 0.5)) %>%
+    config(displaylogo = F, displayModeBar = TRUE,
+           modeBarButtonsToRemove = bttn_remove )
+  
+  
+  
+  return(p)
+  
+}
+
+# Hospital Admissions LOS plot
+make_hospital_admissions_los_plot <- function(data){
+  
+  table <- data %>%
+    arrange(desc(AdmissionWeekEnding)) %>%
+    mutate(AdmissionWeekEnding = convert_opendata_date(AdmissionWeekEnding),
+           Percent = ProportionOfAdmissions*100) %>%
+    select(AdmissionWeekEnding, AgeGroup, LengthOfStay, Percent) %>%
+    dplyr::rename(`Week Ending` = AdmissionWeekEnding,
+                  `Age Group` = AgeGroup,
+                  `Length of Stay` = LengthOfStay)
+
+  table = table %>%
+    filter(`Age Group` == input$los_age) %>%
+    mutate(`Length of Stay` = factor(`Length of Stay`,
+                                     levels = c("1 day or less", "2-3 days", "4-5 days",
+                                                "6-7 days", "8+ days"))) 
+
+  tooltip_trend <- paste0("Week Ending: ", format(table$`Week Ending`, "%d %b %y"), "<br>",
+                        "Length of Stay: ", table$`Length of Stay`, "<br>",
+                        "Percent: ", round(table$Percent, 1), "%")
+
+  table %>%
+    plot_ly(x = ~`Week Ending`,
+            y = ~Percent,
+            color = ~`Length of Stay`,
+            type = 'bar',
+            colors = c(phs_colours("phs-graphite"),
+                       phs_colours("phs-green"),
+                       phs_colours("phs-purple"),
+                       phs_colours("phs-magenta"),
+                       phs_colours("phs-blue")),
+            text = tooltip_trend,
+            hoverinfo = "text",
+            marker = list(line = list(width=.5,
+                                      color = 'rgb(0,0,0)'))) %>%
+    layout(barmode = "stack",
+           yaxis = list(title = 'Percentage of Admissions',
+                        ticksuffix = "%"),
+           xaxis = list(title = 'Admission Date by Week Ending')) %>%
+    # leaving only save plot button
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove )
+  
+}
+
+######################
+### ICU ADMISSIONS ###
+######################
+
+# Daily ICU admissions plot
+make_icu_admissions_plot <- function(data){
+  
+  # Wrangle Data
+  data <- data %>%
+    arrange(desc(DateFirstICUAdmission)) %>%
+    mutate(DateFirstICUAdmission = convert_opendata_date(DateFirstICUAdmission)) %>%
+    select(DateFirstICUAdmission, NewCovidAdmissionsPerDay, SevenDayAverage)
+  
+  # Create axis titles
+  yaxis_title <- "Number of ICU admissions"
+  xaxis_title <- ""
+  
+  #Modifying standard layout
+  yaxis_plots[["title"]] <- yaxis_title
+  xaxis_plots[["title"]] <- xaxis_title
+  
+  #Text for tooltip
+  tooltip_trend <- c(paste0("Date: ", format(data$DateFirstICUAdmission, "%d %b %y"),
+                            "<br>", "ICU Admissions: ", data$NewCovidAdmissionsPerDay,
+                            "<br>", "7 Day Average: ", format(data$SevenDayAverage, nsmall=0, digits=3)))
+  
+
+  #Creating time trend plot
+  p <- plot_ly(data, x = ~DateFirstICUAdmission) %>%
+    add_lines(y = ~NewCovidAdmissionsPerDay, line = list(color = '#0078D4', width=0.8),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Count") %>%
+    add_lines(y = ~SevenDayAverage, line = list(color = '#000000'),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "7 day average") %>%
+    #Layout
+    layout(margin = list(b = 80, t = 5), #to avoid labels getting cut out
+           yaxis = yaxis_plots, xaxis = xaxis_plots,
+           legend = list(x = 100, y = 0.5)) %>% #position of legend
+    # leaving only save plot button
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove )
+  
+  return(p)
+  
+}
+
+########################################
+### HOSPITAL ADMISSIONS BY ETHNICITY ###
+########################################
+
+# Hospital Admissions by Ethnicity Plot
+make_hospital_admissions_ethnicity_plot <- function(data){
+  
+  yaxis_title <- "COVID-19 Admissions"
+  
+  yaxis_plots[["title"]] <- yaxis_title
+  
+  # Star out any NAs for tooltip trend
+  data2 <- data %>%
+    mutate(across(.cols = starts_with(c("admissions", "percentage")), ~ ifelse(is.na(.), "*", as.character(.))))
+  
+  #Text for tooltip
+  tooltip_trend <- c(paste0("Month: ", format(data2$month_begining, "%b %Y"),
+                            "<br>",yaxis_title, " - White: ", data2$admissions_white, " (", data2$percentage_white,"%)",
+                            "<br>",yaxis_title, " - Black/Caribbean: ", data2$admissions_caribbean_or_black," (", data2$percentage_caribbean_or_black,"%)",
+                            "<br>",yaxis_title, " - Mixed/Multiple Ethnic Groups: ", data2$admissions_mixed_or_multiple_ethnic_groups," (", data2$percentage_mixed_or_multiple_ethnic_groups,"%)",
+                            "<br>",yaxis_title, " - African: ", data2$admissions_african," (", data2$percentage_african,"%)",
+                            "<br>",yaxis_title, " - Asian/Asian Scottish/Asian British: ", data2$admissions_asian_asian_scottish_or_asian_british," (", data2$percentage_asian_asian_scottish_or_asian_british,"%)",
+                            "<br>",yaxis_title, " - Other: ", data2$admissions_other," (", data2$percentage_other,"%)",
+                            "<br>",yaxis_title, " - Unknown: ", data2$admissions_unknown," (", data2$percentage_unknown,"%)"))
+  
+  #Creating time trend plot
+  plot_ly(data = data, x = ~month_begining) %>%
+    add_lines(y = ~data$admissions_white, line = list(color = phs_colors("phs-purple")),
+              text = tooltip_trend, hoverinfo="text",
+              name = "White") %>%
+    add_lines(y = ~data$admissions_caribbean_or_black, line = list(color = phs_colors("phs-magenta")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Black/Caribbean") %>%
+    add_lines(y = ~data$admissions_mixed_or_multiple_ethnic_groups, line = list(color = phs_colors("phs-teal")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Mixed/Multiple Ethnic Groups") %>%
+    add_lines(y = ~data$admissions_african, line = list(color = phs_colors("phs-rust")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "African") %>%
+    add_lines(y = ~data$admissions_asian_asian_scottish_or_asian_british, line = list(color = phs_colors("phs-green")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Asian/Asian Scottish/Asian British") %>%
+    add_lines(y = ~data$admissions_other, line = list(color = phs_colors("phs-liberty")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Other") %>%
+    add_lines(y = ~data$admissions_unknown, line = list(color = phs_colors("phs-blue")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Unknown") %>%
+    #Layout
+    layout(margin = list(b = 80, t = 5), #to avoid labels getting cut out
+           yaxis = yaxis_plots, xaxis = xaxis_plots,
+           legend = list(x = 100, y = 0.5)) %>% #position of legend
+    # leaving only save plot button
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove )  
+}
+
+# Hospital Admissions by Ethnicity (Percentage) Plot
+make_hospital_admissions_ethnicity_perc_plot <- function(data){
+  
+  yaxis_title <- "% of COVID-19 Admissions"
+  
+  yaxis_plots[["title"]] <- yaxis_title
+  
+  # Star out any NAs for tooltip trend
+  data2 <- data %>%
+    mutate(across(.cols = starts_with(c("admissions", "percentage")), ~ ifelse(is.na(.), "*", as.character(.))))
+  
+  #Text for tooltip
+  tooltip_trend <- c(paste0("Month: ", format(data2$month_begining, "%b %Y"),
+                            "<br>COVID-19 Admissions -  White: ", data2$admissions_white, " (", data2$percentage_white,"%)",
+                            "<br>COVID-19 Admissions -  Black/Caribbean: ", data2$admissions_caribbean_or_black," (", data2$percentage_caribbean_or_black,"%)",
+                            "<br>COVID-19 Admissions -  Mixed/Multiple Ethnic Groups: ", data2$admissions_mixed_or_multiple_ethnic_groups," (", data2$percentage_mixed_or_multiple_ethnic_groups,"%)",
+                            "<br>COVID-19 Admissions -  African: ", data2$admissions_african," (", data2$percentage_african,"%)",
+                            "<br>COVID-19 Admissions -  Asian/Asian Scottish/Asian British: ", data2$admissions_asian_asian_scottish_or_asian_british," (", data2$percentage_asian_asian_scottish_or_asian_british,"%)",
+                            "<br>COVID-19 Admissions -  Other: ", data2$admissions_other," (", data2$percentage_other,"%)",
+                            "<br>COVID-19 Admissions -  Unknown: ", data2$admissions_unknown," (", data2$percentage_unknown,"%)"))
+  
+  #Creating time trend plot
+  plot_ly(data = data, x = ~month_begining) %>%
+    add_lines(y = ~data$percentage_white, line = list(color = phs_colors("phs-purple")),
+              text = tooltip_trend, hoverinfo="text",
+              name = "White") %>%
+    add_lines(y = ~data$percentage_caribbean_or_black, line = list(color = phs_colors("phs-magenta")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Black/Caribbean") %>%
+    add_lines(y = ~data$percentage_mixed_or_multiple_ethnic_groups, line = list(color = phs_colors("phs-teal")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Mixed/Multiple Ethnic Groups") %>%
+    add_lines(y = ~data$percentage_african, line = list(color = phs_colors("phs-rust")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "African") %>%
+    add_lines(y = ~data$percentage_asian_asian_scottish_or_asian_british, line = list(color = phs_colors("phs-green")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Asian/Asian Scottish/Asian British") %>%
+    add_lines(y = ~data$percentage_other, line = list(color = phs_colors("phs-liberty")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Other") %>%
+    add_lines(y = ~data$percentage_unknown, line = list(color = phs_colors("phs-blue")),
+              text = tooltip_trend, hoverinfo = "text",
+              name = "Unknown") %>%
+    #Layout
+    layout(margin = list(b = 80, t = 5), #to avoid labels getting cut out
+           yaxis = yaxis_plots, xaxis = xaxis_plots,
+           legend = list(x = 100, y = 0.5)) %>% #position of legend
+    # leaving only save plot button
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove )
+  
+}
+
---
+++ shiny_app/indicators/hospital_admissions/hospital_admissions_ui.R
@@ -0,0 +1,126 @@
+tagList(
+  fluidRow(width = 12,
+           h1("COVID-19 hospital admissions"),
+           linebreaks(2)),
+  
+  fluidRow(width = 12,
+           tabBox(width = NULL,
+                  type = "pills",
+                  tabPanel("Hospital Admissions",
+                           tagList(h3("Number of COVID-19 admissions to hospital"),
+                                   tags$div(class = "headline",
+                                            h3(glue("Figures from week ending {Admissions_AgeBD %>% tail(1) %>%
+                                                    .$WeekOfAdmission %>% 
+                                                    format('%d %b %y')}")),
+                                            valueBox(value = {Admissions_AgeBD %>% tail(1) %>%
+                                                .$TotalInfections %>%
+                                                format(big.mark = ",")},
+                                                subtitle = "Latest 7 day total",
+                                                color = "blue",
+                                                icon = icon_no_warning_fn("arrow-up")),
+                                            valueBox(value = {Admissions_AgeSex %>% tail(1) %>%
+                                                .$TotalInfections %>%
+                                                format(big.mark = ",")},
+                                                subtitle = "Cumulative total",
+                                                color = "blue",
+                                                icon = icon_no_warning_fn("hospital-user")),
+                                            # These linebreaks are here to make the banner big enough to
+                                            # include all the valueBoxes
+                                            linebreaks(6))),
+                           
+                           tagList(h3("Daily number of COVID-19 hospital admissions")),
+                           
+                           tabBox(width = NULL, type = "pills",
+                                  tabPanel("Plot",
+                                           tagList(plotlyOutput("hospital_admissions_plot"))),
+                                  tabPanel("Data",
+                                           tagList(dataTableOutput("hospital_admissions_table")))
+                                  
+                           ),
+                           
+                           tagList(h3("Weekly number of COVID-19 hospital admissions by deprivation category (SIMD)")),
+                           
+                           tabBox(width = NULL, type = "pills",
+                                  tabPanel("Plot",
+                                           tagList(plotlyOutput("hospital_admissions_simd_plot"))),
+                                  tabPanel("Data",
+                                           tagList(dataTableOutput("hospital_admissions_simd_table")))
+                                  
+                           ),
+                           
+                           tagList(h3("Length of stay of acute COVID-19 hospital admissions")),
+                           
+                           tabBox(width = NULL, type = "pills",
+                                  tabPanel("Plot",
+                                           tagList(h5("Use the drop-down menu to select an age group of interest."),
+                                                   h5("Please note that in cases where there are no hospital admissions, there will be a gap in the chart."),
+                                             column(width = 3,
+                                                    selectInput(inputId = "los_age", width = "100%",
+                                                         label = "Select Age Group",
+                                                         choices = {Length_of_Stay %>%
+                                                             arrange(AgeGroup) %>%
+                                                             .$AgeGroup %>%
+                                                             unique()},
+                                                         selected = "All Ages")),
+                                             column(width = 9,
+                                                    plotlyOutput("hospital_admissions_los_plot")))),
+                                  tabPanel("Data",
+                                           tagList(dataTableOutput("hospital_admissions_los_table")))
+                                  
+                           )
+                           
+                           ),
+                  
+                  tabPanel("Hospital Admissions by Ethnicity",
+                           tagList(h3("Admissions to hospital 'with' COVID-19 by ethnicity"),
+                                   h4(strong("These data will next be updated in March 2023.")),
+                           tabBox(width = NULL, type = "pills",
+                                  tabPanel("Plot",
+                                           tagList(h5("Click on legend to select or deselect categories:"),
+                                                   h5("\U2022 Single click on an item in the legend to remove it from the plot"),
+                                                   h5("\U2022 Double click on an item in the legend to view only that line"),
+                                             br(),
+                                             h3("Admissions to hospital 'with' COVID-19 by ethnicity - Cases"),      
+                                             plotlyOutput("hospital_admissions_ethnicity_plot"),
+                                             h3("Admissions to hospital 'with' COVID-19 by ethnicity - Percentage"),
+                                             plotlyOutput("hospital_admissions_ethnicity_perc_plot"))),
+                                  tabPanel("Data",
+                                           dataTableOutput("hospital_admissions_ethnicity_table"))))),
+                               
+
+                  tabPanel("ICU Admissions",
+                           tagList(h3("Number of COVID-19 admissions to ICU"),
+                                   tags$div(class = "headline",
+                                            h3(glue("Figures from week ending {ICU %>% tail(1) %>%
+                                                    .$DateFirstICUAdmission %>% 
+                                                    convert_opendata_date() %>%
+                                                    format('%d %b %y')}")),
+                                            valueBox(value = {ICU %>% tail(7) %>%
+                                                .$NewCovidAdmissionsPerDay %>% 
+                                                sum() %>%
+                                                format(big.mark = ",")},
+                                                subtitle = "Latest 7 day total",
+                                                color = "blue",
+                                                icon = icon_no_warning_fn("arrow-up")),
+                                            valueBox(value = {ICU_AgeSex %>% tail(1) %>%
+                                                .$CovidAdmissionsToICU %>%
+                                                format(big.mark = ",")},
+                                                subtitle = "Cumulative total",
+                                                color = "blue",
+                                                icon = icon_no_warning_fn("bed-pulse")),
+                                            # These linebreaks are here to make the banner big enough to
+                                            # include all the valueBoxes
+                                            linebreaks(6))),
+                           tagList(h3("Daily number of COVID-19 ICU admissions")),
+                           
+                           tabBox(width = NULL, type = "pills",
+                                  tabPanel("Plot",
+                                           tagList(plotlyOutput("icu_admissions_plot"))),
+                                  tabPanel("Data",
+                                           tagList(dataTableOutput("icu_admissions_table")))
+                                  
+                           ))
+           )
+  )
+  
+)
