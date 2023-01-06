3306b1d45df25b49305ff46faa8890fc2e5ec679
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-06 11:57:16 +0000

    using list of tags as content, and example for first plot

--- shiny_app/alt-text/alt_text_modals_server.R
+++ shiny_app/alt-text/alt_text_modals_server.R
@@ -8,7 +8,7 @@ altTextServer <- function(id, title, content) {
 
       modal <- modalDialog(
         h3(title),
-        p(content),
+        content,
         size = "l",
         easyClose = TRUE,
         fade = TRUE,
@@ -32,54 +32,66 @@ altTextServer <- function(id, title, content) {
 #      in indicators/cases/cases_ui.R
 ons_cases_modal <- altTextServer("ons_cases_modal",
                                  title = "Estimated COVID-19 infection rate",
-                                 content = "This plot shows the ONS covid infection ...")
+                                 content = tags$ul(tags$li("This is a plot of the estimated COVID-19 infection rate in the population",
+                                                           "from the Office for National Statistics."),
+                                                   tags$li("The x axis shows week ending, starting from 06 November 2020."),
+                                                   tags$li("The y axis shows the official positivity estimate, as a percentage",
+                                                           "of the population in Scotland. "),
+                                                   tags$li("There is one trace which includes error bars denoting confidence intervals."),
+                                                   tags$li("The positivity estimate peaked at 1 in 11 for week ending 20 Mar 2022."),
+                                                   tags$li("The latest positivity estimate in week ending",
+                                                           glue("{ONS %>% tail(1) %>% .$EndDate %>% convert_opendata_date() %>% format('%d %b %y')}"),
+                                                           glue("is {ONS %>% tail(1) %>% .$EstimatedRatio}")
+                                                           )
+                                                   )
+                                 )
 
 r_number_modal <- altTextServer("r_number_modal",
                                       title = "Estimated COVID-19 R number",
-                                      content = "This plot shows ...")
+                                content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 wastewater_modal <- altTextServer("wastewater_modal",
                                 title = "Seven day average trend in wastewater COVID-19",
-                                content = "This plot shows ...")
+                                content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 reported_cases_modal <- altTextServer("reported_cases_modal",
                                       title = "Reported COVID-19 cases",
-                                      content = "This plot shows ...")
+                                      content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 hospital_admissions_modal <- altTextServer("hospital_admissions_modal",
                                            title = "Daily number of COVID-19 hospital admissions",
-                                           content = "This plot shows ... ")
+                                           content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 hospital_admissions_simd_modal <- altTextServer("hospital_admissions_simd_modal",
                                            title = "Weekly number of COVID-19 hospital admissions by deprivation category (SIMD)",
-                                           content = "This plot shows ... ")
+                                           content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 hospital_admissions_los_modal <- altTextServer("hospital_admissions_los_modal",
                                            title = "Length of stay of acute COVID-19 hospital admissions",
-                                           content = "This plot shows ... ")
+                                           content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 hospital_admissions_ethnicity_modal <- altTextServer("hospital_admissions_ethnicity_modal",
                                                title = "Admissions to hospital 'with' COVID-19 by ethnicity",
-                                               content = "This plot shows ... ")
+                                               content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 icu_admissions_modal <- altTextServer("icu_admissions_modal",
                                        title = "Daily number of COVID-19 ICU admissions",
-                                       content = "This plot shows ... ")
+                                      content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 hospital_occupancy_modal <- altTextServer("hospital_occupancy_modal",
                                       title = "Number of patients with COVID-19 in hospital",
-                                      content = "This plot shows ... ")
+                                      content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 icu_occupancy_modal <- altTextServer("icu_occupancy_modal",
                                       title = "Number of patients with COVID-19 in ICU",
-                                      content = "This plot shows ... ")
+                                     content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 vaccine_wastage_modal <- altTextServer("vaccine_wastage_modal",
                                      title = "COVID-19 vaccine wastage",
-                                     content = "This plot shows ... ")
+                                     content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 vaccine_wastage_reason_modal <- altTextServer("vaccine_wastage_reason_modal",
                                        title = "Reasons for COVID-19 vaccine wastage",
-                                       content = "This plot shows ... ")
+                                       content = tags$ul(tags$li("point a"), tags$li("point b"), tags$li("point c")))
 
 
