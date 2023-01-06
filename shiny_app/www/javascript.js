0b963ac3599bec130fee7dbd1832651ca5c69628
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-06 11:32:23 +0000

    setting focus on modal close

--- shiny_app/www/javascript.js
+++ shiny_app/www/javascript.js
@@ -8,3 +8,10 @@ $(document).on('shown.bs.modal', (x) => {
   $('button').focus();
 
 });
+
+$(document).on('hide.bs.modal', (x) => {
+  // With no more specific selector focus is given
+  // to the last button on the page which is the modal button
+  $('button').focus();
+
+});
