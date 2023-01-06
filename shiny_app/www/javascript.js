416041676e3d249c4e381d898e7d9380d092ea56
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-06 11:42:01 +0000

    remove comment

--- shiny_app/www/javascript.js
+++ shiny_app/www/javascript.js
@@ -10,8 +10,5 @@ $(document).on('shown.bs.modal', (x) => {
 });
 
 $(document).on('hide.bs.modal', (x) => {
-  // With no more specific selector focus is given
-  // to the last button on the page which is the modal button
   $('button').focus();
-
 });
