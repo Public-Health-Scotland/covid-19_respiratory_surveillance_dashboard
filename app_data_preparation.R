b02c4b2b5dc805f0ce40b8d7c80ed6f9a3db4c6f
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-09 09:01:08 +0000

    copy over dictionary files in app data prep

--- app_data_preparation.R
+++ app_data_preparation.R
@@ -16,6 +16,7 @@ if (!is.null(project_directory)){ setwd(project_directory) }
 dir.create(here::here("shiny_app", "data"))
 
 output_folder <- "/conf/C19_Test_and_Protect/Test & Protect - Warehouse/Weekly Covid Dashboard/Output/"
+data_dictionary_folder <- "/conf/C19_Test_and_Protect/Test & Protect - Warehouse/Weekly Covid Dashboard/Data Dictionaries/"
 shiny_data_folder <- "shiny_app/data/"
 
 
@@ -23,14 +24,21 @@ shiny_data_folder <- "shiny_app/data/"
 purrr::walk(list.files(path=shiny_data_folder, full.names=TRUE), file.remove)
 
 # Load all processed data files and save out as rds
-copy_to_shiny_data <- function(csv){
-  # Given a .csv file name in processed_data_folder
+copy_to_shiny_data <- function(csv, startloc){
+  # Given a .csv file name in startloc
   # copies it across to an .rds file in shiny_data_folder
-  readfile <- readr::read_csv(paste0(output_folder, csv))
+  readfile <- readr::read_csv(paste0(startloc, csv))
   saveRDS(readfile, paste0(shiny_data_folder, gsub(".csv", ".rds", csv)))
 }
 
 # Copy all csv files in processed_data to shiny_data as rds files
 # Not copying march files across as only June needed
 files =  list.files(path=output_folder)
-purrr::walk(files, copy_to_shiny_data)
+purrr::walk(files, copy_to_shiny_data, startloc = output_folder)
+
+# Now get all data dictionaries
+dictionary_files = list.files(path=data_dictionary_folder)
+purrr::walk(dictionary_files, copy_to_shiny_data, startloc = data_dictionary_folder)
+
+
+
