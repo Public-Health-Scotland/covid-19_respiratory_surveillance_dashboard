842341bee4da587cca56d44866494bd73f06b43c
Calum Massie <calumm07@nssrstudio08.csa.scot.nhs.uk>
2023-01-13 15:43:30 +0000

    Adding instructions on using dashboard
    Explaining accessibility features

--- shiny_app/indicators/introduction/introduction_server.R
+++ shiny_app/indicators/introduction/introduction_server.R
@@ -27,23 +27,39 @@ output$introduction_about <- renderUI({
 })
 
 output$introduction_use <- renderUI({
-  tagList(h3(tags$b("Using the dashboard")),
-          p("There are tabs across the top for the each topic area within the dashboard....."),
-          p("Note that some numbers may not sum to the total as disclosure control methods have been applied
-            to the data in order to protect patient confidentiality."), br(),
-
-          p(tags$b("Interacting with the dashboard")),
-          p(tags$li("On each tab there are........")),
-          p(tags$li("On the line charts,
-                    clicking on a category in the legend will remove it from the chart. This is useful to reduce the number of lines
-                    on the chart and makes them easier to see. A further click on the categories will add them back into the chart.")),
-          p(tags$li("There are.....")), br(),
+  tagList(h3(tags$b("Interacting with the dashboard")),
+          p("Click on tabs in the navigation bar at the top to view each topic. Click on 'Summary' to view the most recently available
+            main statistics. Click on 'Metadata' to view notes about the data. Click on 'Download data' to download datasets as an Excel or CSV file."),
+          br(),
+          p(tags$b("Within each tab")),
+          p(tags$li("Click the 'Metadata' button to navigate to relevant notes")),
+          p(tags$li("Click on the toggles to change the visible sub-topic")),
+          p(tags$li("Summary banners display the most recently available headline figures")),
+          p(tags$li("Click 'Plot/Data' toggle to switch between chart and data table view")),
+          br(),
+          p(tags$b("Interacting with the charts")),
+          p(tags$li("Click the 'Plot description' button for a summary of the plot content")),
+          p(tags$li("Move the cursor over the data points to see the data values")),
+          p(tags$li("Zoom into the plot by holding down the cursor and dragging to select the region")),
+          p(tags$li("Alter the x axis range by dragging the vertical white bars on the left and right of the bottom panel")),
+          p(tags$li("Click the home button in the top right to reset the axes")),
+          p(tags$li("Single click on legend variables to remove the corresponding trace")),
+          p(tags$li("Double click on legend variables to isolate the corresponding trace")),
+          p(tags$li("Double click on the legend to restore all traces")),
+          p(tags$li("Click the camera icon in the top right to download the plot as a png file")),
+          br(),
           p(tags$b("Downloading data")),
-          p(tags$li("Some of the data is also available on the ",
-                    tags$a(href="https://www.opendata.nhs.scot/dataset/hospital-standardised-mortality-ratios",
+          p(tags$li("Data can be downloaded in open data format from the 'Download data' tab as well as from the",
+                    tags$a(href="https://www.opendata.nhs.scot",
                            "Scottish Health and Social Care Open Data platform (external website).", target="_blank"))),
+          p(tags$li("On the 'Download data' tab you can select the dataset and file type you wish to download")),
+          p(tags$li("You can view a data summary and a data preview before downloading")),
           p(tags$li("To download an image of any of the charts in the dashboard, click the camera icon in the top-right
-                    corner of the chart and a png image file will automatically download."))
+                    corner of the chart and a png image file will automatically download.")),
+          br(),
+          p(tags$b("Disclosure")),
+          p("Note that some numbers may not sum to the total as disclosure control methods have been applied
+            to the data in order to protect patient confidentiality.")
           ) #tagList
 })
 
@@ -75,11 +91,21 @@ output$introduction_accessibility <- renderUI({
 
           p(tags$b("Compliance status")),
           p("This site has not yet been evaluated against Web Content Accessibility Guidelines
-                                   version 2.1 level AA standard."),
-
+                                   version 2.1 level AA standard. We are currently working towards an evaluation."),
+          br(),
+          p(tags$b("Accessible features")),
+          p("We have taken the following steps to make this dashboard more accessible:"),
+          p(tags$li("Colours have been chosen to meet colour contrast standards")),
+          p(tags$li("Plot colours have been chosen to be colour blind friendly")),
+          p(tags$li("The dashboard can be navigated using a keyboard")),
+          p(tags$li("The content is still readable at 400% zoom")),
+          p(tags$li("Alternative descriptions of plot contents have been provided via 'Plot description' buttons")),
+          p(tags$li("Data tables have been provided alongside plots where possible")),
+          p(tags$li("Hover content has been replaced with clickable content where possible")),
+          br(),
           p(tags$b("Reporting any accessibility problems with this website")),
           p("If you wish to contact us about any accessibility issues you encounter on this
-                                   site, please email ", tags$a(href="mailto:phs.qualityindicators@phs.scot", "phs.qualityindicators@phs.scot", ".")),
+                                   site, please email ", tags$a(href="mailto:phs.covid19data&analytics@phs.scot", "phs.covid19data&analytics@phs.scot", ".")),
 
           p(tags$b("Enforcement procedure")),
           p("The Equality and Human Rights Commission (EHRC) is responsible for enforcing the
@@ -89,7 +115,7 @@ output$introduction_accessibility <- renderUI({
                    target = "_blank")),
 
           p(tags$b("Preparation of this accessibility statement")),
-          p("This statement was prepared on 15 June 2022. It was last reviewed on 08 December 2022.")
+          p("This statement was prepared on 13 January 2023. It was last reviewed on 13 January 2023.")
   ) # tagList
 })
 
