5d3da84b02cbeff557451fa2be0dc63b17cb5c51
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-19 12:49:11 +0000

    forgot the new module!

---
+++ shiny_app/modules/metadata_button/metadata_button_server.R
@@ -0,0 +1,33 @@
+#### Metadata button server ----
+
+
+metadataButtonServer <- function(id, panel, parent){
+
+  moduleServer(id,
+               function(input, output, session){
+
+                 observeEvent(input$jump_to_metadata,
+                              {updateTabsetPanel(session = parent, "intabset", selected = "metadata")
+                               updateCollapse(session = parent, "notes_collapse", open = panel)
+                                })
+
+               })
+
+
+}
+
+cases_metadata_button <- metadataButtonServer(id="cases",
+                                              panel="Cases",
+                                              parent = session)
+
+hospital_admissions_metadata_button <- metadataButtonServer(id="hospital_admissions",
+                                                            panel="Hospital admissions",
+                                                            parent = session)
+
+hospital_occupancy_metadata_button <- metadataButtonServer(id="hospital_occupancy",
+                                                           panel="Hospital occupancy",
+                                                           parent = session)
+
+cases_metadata_button <- metadataButtonServer(id="vaccines",
+                                              panel="Vaccine wastage",
+                                              parent = session)
