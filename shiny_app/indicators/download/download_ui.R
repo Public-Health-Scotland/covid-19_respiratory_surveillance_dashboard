6e14acd18e6083a876e450e99375e09cf518ef73
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-20 11:49:02 +0000

    Download ui Set title reduce width of pickerinput radiobuttons for filetype

--- shiny_app/indicators/download/download_ui.R
+++ shiny_app/indicators/download/download_ui.R
@@ -1,8 +1,11 @@
 tagList(
   div(
 
-    fluidRow(width=12,
-             br()),
+    fluidRow(width = 12,
+             metadataButtonUI("download"),
+             h1("Download data"),
+             linebreaks(2)),
+
 
     # Filters and toggles
     fluidRow(width=12,
@@ -23,19 +26,15 @@ tagList(
                                selected = NULL,
                                inline = TRUE,
                                multiple = FALSE,
-                               width = "100%",
                                options = pickerOptions(
                                  showTick = TRUE)
                    ),
-                   pickerInput("download_filetype",
+                   linebreaks(1),
+                   radioButtons("download_filetype",
                                "3. Choose file type   ",
                                choices = c(".csv", ".xlsx"),
                                selected = ".csv",
-                               inline = TRUE,
-                               multiple = FALSE,
-                               width = "100%",
-                               options = pickerOptions(
-                                 showTick = TRUE)
+                               inline = TRUE
                    )
 
                  )
