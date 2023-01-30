e8166870ff5f2254a45cb5442dd0ece666df4377
RosalynLP <rosalyn.pearson@phs.scot>
2023-01-30 13:54:18 +0000

    fix sorting on data tables

--- shiny_app/functions/core_functions.R
+++ shiny_app/functions/core_functions.R
@@ -50,12 +50,13 @@ format_entry <- function(x, dp=0, perc=F){
 
 # Data table for Data tab ----
 make_table <- function(input_data_table,
-                          add_separator_cols = NULL, # with , separator and 0dp
-                          add_separator_cols_1dp = NULL, # with , separator and 1dp
-                          add_percentage_cols = NULL, # with % symbol and 2dp
-                          maxrows = 14, # max rows displayed on page
-                          order_by_firstcol = NULL, # asc, desc or NULL
-                          highlight_column = NULL # Column to highlight specific entries based off
+                       add_separator_cols = NULL, # with , separator and 0dp
+                       add_separator_cols_1dp = NULL, # with , separator and 1dp
+                       add_percentage_cols = NULL, # with % symbol and 2dp
+                       maxrows = 14, # max rows displayed on page
+                       order_by_firstcol = NULL, # asc, desc or NULL
+                       filter_cols = NULL, # columns to have filters for
+                       highlight_column = NULL # Column to highlight specific entries based off
 ){
 
 
@@ -77,6 +78,15 @@ make_table <- function(input_data_table,
     input_data_table[i] <- apply(input_data_table[i], MARGIN=1, FUN=format_entry, dp=1, perc=T)
   }
 
+  # Always filter date cols
+  date_cols <- which({purrr::map(input_data_table, class) %>% paste()} == "Date")
+  filter_cols <- union(filter_cols, date_cols)
+
+  # Getting columns not to filter based off columns to filter
+  # Shifting down by 1 as data table starts counting from 0 whereas we want
+  # to choose columns to filter by counting from 1 like in R
+  no_filter_cols <- seq(0, (ncol(input_data_table)-1)) %>% .[!(. %in% (filter_cols-1))]
+
   if(!is.null(order_by_firstcol)){
     tab_order <- list(list(0, order_by_firstcol))
   } else {
@@ -89,19 +99,21 @@ make_table <- function(input_data_table,
                       rownames = FALSE,
                       options = list(pageLength = maxrows,
                                      dom = 'tip',
+                                     columnDefs = list(list(searchable = FALSE,
+                                                            targets = no_filter_cols)),
+                                     ordering = FALSE,
                                      scrollX = TRUE,
                                      initComplete = JS(
                                        "function(settings, json) {",
                                        "$(this.api().table().header()).css({'background-color': 'rgba(1, 0, 104, 1)', 'color': 'white'});",
-                                       "}"), # Make header phs-blue
+                                       "}"), # Make header navy
                                      order = tab_order),
-
-                      filter = "top",
+                      filter = list(position="top", clear=TRUE, plain=FALSE),
                       colnames = table_colnames) %>%
     formatStyle(
       highlight_column, target="row",
       backgroundColor = styleEqual(c("Cumulative", "Total"),
-                                   c(phs_colours("phs-magenta"), phs_colours("phs-magenta"))),
+                                   c(phs_colours("phs-blue"), phs_colours("phs-blue"))),
       fontWeight = styleEqual(c("Cumulative", "Total"), c("bold", "bold")),
       color = styleEqual(c("Cumulative", "Total"), c("white", "white"))
     )
