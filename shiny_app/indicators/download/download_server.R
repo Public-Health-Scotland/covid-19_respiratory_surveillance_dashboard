5e9cf93aa2d82180322a41c392e647f427cb8908
Mark Hamilton <markha06@nssrstudio08.csa.scot.nhs.uk>
2023-02-16 13:15:46 +0000

    Fix so open data statement appears for hospital admissions

--- shiny_app/indicators/download/download_server.R
+++ shiny_app/indicators/download/download_server.R
@@ -99,7 +99,7 @@ output$data_download_table <- renderDataTable({
 output$data_download_open_data_statement <- renderUI({
 
   ifelse(input$download_indicator %in% c("COVID-19 cases",
-                                         "COVID-19 hospital admissions",
+                                         "Acute COVID-19 hospital admissions",
                                          "COVID-19 hospital occupancy",
                                          "Vaccine wastage"),
          tagList(p("This dataset follows the ", tags$a(href="https://www.opendata.nhs.scot/uploads/admin/PHS-Open-Data-Standards-Version-1.0.pdf",
