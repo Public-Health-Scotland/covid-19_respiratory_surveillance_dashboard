b800fe1a6bb8201f346f7e447b46609d9bf2f6e9
Jen-e2 <evansjennifer0210@gmail.com>
2023-05-17 09:37:46 +0100

    add notes to occupancy tab

--- shiny_app/indicators/hospital_occupancy/hospital_occupancy_ui.R
+++ shiny_app/indicators/hospital_occupancy/hospital_occupancy_ui.R
@@ -9,52 +9,54 @@ tagList(
 
   fluidRow(width = 12,
            tagList(h2("Number of inpatients with COVID-19 in hospital"),
-                tags$div(class = "headline",
-                                     h3(glue("Hospital occupancy (inpatients) on the Sunday of the latest three weeks available")),
-                                     valueBox(value = {occupancy_headlines[[1]]$HospitalOccupancy %>% format(big.mark=",")},
-                                         subtitle = glue("As at {names(occupancy_headlines)[[1]]}"),
-                                         color = "fuchsia",
-                                         icon = icon_no_warning_fn("calendar-week")),
-                                     valueBox(value = {occupancy_headlines[[2]]$HospitalOccupancy %>% format(big.mark=",")},
-                                         subtitle = glue("As at {names(occupancy_headlines)[[2]]}"),
-                                         color = "fuchsia",
-                                         icon = icon_no_warning_fn("calendar-week")),
-                                     valueBox(value = {occupancy_headlines[[3]]$HospitalOccupancy %>% format(big.mark=",")},
-                                         subtitle = glue("As at {names(occupancy_headlines)[[3]]}"),
-                                         color = "fuchsia",
-                                         icon = icon_no_warning_fn("calendar-week")),
-                                     # This text is hidden by css but helps pad the box at the bottom
-                                    h6("hidden text for padding page"))),
+                   tags$div(class = "headline",
+                            h3(glue("Hospital occupancy (inpatients) on the Sunday of the latest three weeks available")),
+                            valueBox(value = glue("{occupancy_headlines[[1]]$HospitalOccupancy %>% format(big.mark=",")}*"),
+                                     subtitle = glue("As at {names(occupancy_headlines)[[1]]}"),
+                                     color = "fuchsia",
+                                     icon = icon_no_warning_fn("calendar-week")),
+                            valueBox(value = {occupancy_headlines[[2]]$HospitalOccupancy %>% format(big.mark=",")},
+                                     subtitle = glue("As at {names(occupancy_headlines)[[2]]}"),
+                                     color = "fuchsia",
+                                     icon = icon_no_warning_fn("calendar-week")),
+                            valueBox(value = {occupancy_headlines[[3]]$HospitalOccupancy %>% format(big.mark=",")},
+                                     subtitle = glue("As at {names(occupancy_headlines)[[3]]}"),
+                                     color = "fuchsia",
+                                     icon = icon_no_warning_fn("calendar-week")),
+                            h4("*Change to inpatient definition from 08 May 2023 (max number 10 days)"),
+                            # This text is hidden by css but helps pad the box at the bottom
+                            h6("hidden text for padding page"))),
            linebreaks(1)),
 
   fluidRow(
-           tabBox(width = NULL,
-                  type = "pills",
-                  tabPanel("Plot",
-                           tagList(linebreaks(1),
-                                   altTextUI("hospital_occupancy_modal"),
-                                   withNavySpinner(plotlyOutput("hospital_occupancy_plot")),
-                           fluidRow(
-                             width=12, linebreaks(5))
-                                   ) # taglist
-                           ), # tabpanel
+    tabBox(width = NULL,
+           type = "pills",
+           tabPanel("Plot",
+                    tagList(linebreaks(1),
+                            altTextUI("hospital_occupancy_modal"),
+                            withNavySpinner(plotlyOutput("hospital_occupancy_plot")),
+                            fluidRow(
+                              width=12, linebreaks(5))
+                    ) # taglist
+           ), # tabpanel
 
-                  tabPanel("Data",
-                           tagList(h3("Number of inpatients with COVID-19 in hospital data"),
-                                   withNavySpinner(dataTableOutput("hospital_occupancy_table"))
-                                   ) # taglist
-                           ) # tabpanel
-                  ) #tabbox
+           tabPanel("Data",
+                    tagList(h3("Number of inpatients with COVID-19 in hospital data"),
+                            withNavySpinner(dataTableOutput("hospital_occupancy_table"))
+                    ) # taglist
+           ) # tabpanel
+    ) #tabbox
 
-          ), # fluid row
+  ), # fluid row
 
   fluidRow(
-           br()),
+    br()),
 
   fluidRow(width = 12,
            tagList(h2("7 day average number of patients with COVID-19 in Intensive Care Units (ICU)"),
+                   p("From 08 May 2023, manual data collections from NHS Boards on the number of patients in ICU paused. These data are no longer updated. "),
                    tags$div(class = "headline",
-                            h3(glue("Figures from week ending {Occupancy_Hospital %>%
+                            h3(glue("Figures from week ending {Occupancy_ICU %>%
                                               mutate(Date = convert_opendata_date(Date)) %>%
                                               filter(Date <= floor_date(today(), 'week')) %>%
                                               tail(1) %>%
@@ -95,7 +97,7 @@ tagList(
   ), # fluid row
 
   fluidRow(
-           br())
+    br())
 
 ) # taglist
 
