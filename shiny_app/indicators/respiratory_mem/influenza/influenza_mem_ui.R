0255ec37614d6d726972dbedc538f6686a8df69c
markha06 <mark.hamilton4@phs.scot>
2023-08-17 12:44:58 +0100

    Add headline figures for influenza

--- shiny_app/indicators/respiratory_mem/influenza/influenza_mem_ui.R
+++ shiny_app/indicators/respiratory_mem/influenza/influenza_mem_ui.R
@@ -3,8 +3,39 @@ tagList(
            
            metadataButtonUI("respiratory_influenza_mem"),
            linebreaks(1),
-           h1("Influenza Incidence Rates"),
-           linebreaks(1)),
+           h1("Influenza")),
+  
+  fluidRow(width = 12,
+           tabPanel(stringr::str_to_sentence("influenza"),
+           # headline figures for the week in Scotland
+           tagList(h2(glue("Summary of influenza cases in Scotland")),
+                   tags$div(class = "headline",
+                            h3(glue("Total number of influenza cases in Scotland over the last two weeks")),
+                            # this week total number
+                            valueBox(value = {Respiratory_Summary_Totals %>% filter(FluOrNonFlu == "flu") %>%
+                                .$CountThisWeek %>% format(big.mark=",")},
+                                subtitle = glue("Week ending {Respiratory_Summary_Totals %>% filter(FluOrNonFlu == 'flu') %>%
+                                                .$DateThisWeek %>% format('%d %b %y')}"),
+                                color = "teal",
+                                icon = icon_no_warning_fn("calendar-week")),
+                            # previous week total number
+                            valueBox(value = {Respiratory_Summary_Totals %>% filter(FluOrNonFlu == "flu") %>%
+                                .$CountPreviousWeek %>% format(big.mark=",")},
+                                subtitle = glue("Week ending {Respiratory_Summary_Totals %>% filter(FluOrNonFlu == 'flu') %>%
+                                                .$DatePreviousWeek %>% format('%d %b %y')}"),
+                                color = "teal",
+                                icon = icon_no_warning_fn("calendar-week")),
+                            # percentage difference between the previous weeks
+                            valueBox(value = glue("{Respiratory_Summary_Totals %>% filter(FluOrNonFlu == 'flu') %>%
+                                                  .$PercentageDifference}%"),
+                                     subtitle = glue("{Respiratory_Summary_Totals %>% filter(FluOrNonFlu == 'flu') %>%
+                                                     .$ChangeFactor %>% str_to_sentence()} in the last week"),
+                                     color = "teal",
+                                     icon = icon_no_warning_fn({flu_icon_headline %>% filter(FluOrNonFlu == 'flu') %>%
+                                         .$icon})),
+                            # This text is hidden by css but helps pad the box at the bottom
+                            h6("hidden text for padding page")
+                   )))), # headline
   
   fluidRow(width = 12,
            tagList(h2("Influenza incidence rate per 100,000 population in Scotland"))),
