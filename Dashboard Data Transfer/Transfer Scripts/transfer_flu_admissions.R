d8be2f3866fc56a8b81d723ad018ba96da032f7a
calumm07 <calum.massie2@phs.scot>
2023-09-19 15:25:52 +0100

    changing data transfer to be open data format and have season

--- Dashboard Data Transfer/Transfer Scripts/transfer_flu_admissions.R
+++ Dashboard Data Transfer/Transfer Scripts/transfer_flu_admissions.R
@@ -3,11 +3,25 @@
 
 ##### Influenza
 
-i_influenza <- read_csv_with_options(glue(input_data, "admissions_flu_{format(report_date -1,'%Y-%m-%d')}.csv"))
+date_reference <-readRDS("/conf/C19_Test_and_Protect/Analyst Space/Calum (Analyst Space)/flu_seasons.Rds") %>%
+  distinct(date, year, ISOweek, flu_season)
 
-g_influenza <- i_influenza %>%
-  dplyr::rename(admissions = Frequency) 
+i_influenza_admissions <- read_csv_with_options(match_base_filename(glue(input_data, "admissions_flu.csv")))
 
-write.csv(g_influenza, glue(output_folder, "Influenza_admissions.csv"), row.names = FALSE)
+g_influenza_admissions <- i_influenza %>%
+  dplyr::rename(Admissions = Frequency,
+                Date = date_plot) %>%
+  mutate(date = as.Date(Date)) %>%
+  left_join(date_reference) %>%
+  mutate(Date = as_date(ceiling_date(as_date(Date), "week",
+                                                 change_on_boundary = F))) %>%
+  dplyr::rename(FluType = Flu_type_AB,
+                RatePer100000 = Rate_per_100000,
+                Year = year,
+                ISOWeek = ISOweek,
+                Season = flu_season) %>%
+  select(Date, Year, ISOWeek, Season, FluType, Admissions, RatePer100000)
 
-rm(i_influenza, g_influenza)
+write.csv(g_influenza_admissions, glue(output_folder, "Influenza_admissions.csv"), row.names = FALSE)
+
+rm(i_influenza_admissions, g_influenza_admissions)
