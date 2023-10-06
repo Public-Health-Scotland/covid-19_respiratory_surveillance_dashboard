782023db749bdc0ae3dcfb0bfcf7c47ab61aefc8
iainma09 <iain.mackinnon2@phs.scot>
2023-10-06 15:41:01 +0100

    Adenovirus, HMPV, Mycoplasma Text at top of tab – remove last sentence until the hyperlink is live All charts – data tables – change to 1.dp All charts - update plot descriptions with ISO week bullet and MEM thresholds as per Stef's email

--- shiny_app/indicators/respiratory_mem/hmpv/hmpv_mem_server.R
+++ shiny_app/indicators/respiratory_mem/hmpv/hmpv_mem_server.R
@@ -56,8 +56,8 @@ seasons <- seasons$Season
 altTextServer("hmpv_mem_modal",
               title = "HMPV incidence rate per 100,000 population",
               content = tags$ul(tags$li("This is a plot showing the rate of HMPV infection per 100,000 population in Scotland."),
-                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ", 
-                                        "The first ISO week is the first week of the year (in January) and the 52nd ISO week is the last week of the year."),
+                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ",
+                                        "Week 40 is typically the start of October and when the influenza season starts."),
                                 tags$li("The y axis shows the rate of influenza infection per 100,000 population."),
                                 tags$li(glue("There is a trace for each of the following seasons: ", seasons[1], ", ",
                                              seasons[2], ", ", seasons[3], ", ", seasons[4], ", ", seasons[5], ", ",
@@ -74,8 +74,8 @@ altTextServer("hmpv_mem_hb_modal",
               title = "HMPV incidence rate per 100,000 population by NHS Health Board",
               content = tags$ul(tags$li(glue("This is a plot showing the rate of HMPV infection per 100,000 population by NHS Health Board for seasons ",
                                              seasons[6], " and ", seasons[7], ".")),
-                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ", 
-                                        "The first ISO week is the first week of the year (in January) and the 52nd ISO week is the last week of the year."),
+                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ",
+                                        "Week 40 is typically the start of October and when the influenza season starts."),
                                 tags$li("The y axis shows the NHS Health Board."),
                                 tags$li("Each cell is coloured according to the activity level: Baseline, Low, Moderate, High, or Extraordinary."),
                                 tags$li("Caution should be taken when interpreting the activity levels (and MEM thresholds) for smaller NHS Health Boards. ",
@@ -88,8 +88,8 @@ altTextServer("hmpv_mem_age_modal",
               title = "HMPV incidence rate per 100,000 population by age group",
               content = tags$ul(tags$li(glue("This is a plot showing the rate of HMPV infection per 100,000 population by age group for seasons ",
                                              seasons[6], " and ", seasons[7], ".")),
-                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ", 
-                                        "The first ISO week is the first week of the year (in January) and the 52nd ISO week is the last week of the year."),
+                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ",
+                                        "Week 40 is typically the start of October and when the influenza season starts."),
                                 tags$li("The y axis shows the age group."),
                                 tags$li("Each cell is coloured according to the activity level: Baseline, Low, Moderate, High, or Extraordinary."),
                                 tags$li("Caution should be taken when interpreting the activity levels (and MEM thresholds) for smaller age groups. ",
@@ -110,7 +110,7 @@ output$hmpv_mem_table <- renderDataTable({
     rename(`ISO Week` = ISOWeek,
            `Rate per 100,000` = RatePer100000,
            `Activity Level` = ActivityLevel) %>%
-    make_table(add_separator_cols_2dp = c(3),
+    make_table(add_separator_cols_1dp = c(3),
                filter_cols = c(1,2,4))
 })
 
@@ -128,7 +128,7 @@ output$hmpv_mem_hb_table <- renderDataTable({
            `NHS Health Board`= HBName,
            `Rate per 100,000` = RatePer100000,
            `Activity Level` = ActivityLevel) %>%
-    make_table(add_separator_cols_2dp = c(4),
+    make_table(add_separator_cols_1dp = c(4),
                filter_cols = c(1,2,3,5))
 })
 
@@ -146,7 +146,7 @@ output$hmpv_mem_age_table <- renderDataTable({
            `Age Group`= AgeGroup,
            `Rate per 100,000` = RatePer100000,
            `Activity Level` = ActivityLevel) %>%
-    make_table(add_separator_cols_2dp = c(4),
+    make_table(add_separator_cols_1dp = c(4),
                filter_cols = c(1,2,3,5))
 })
 
