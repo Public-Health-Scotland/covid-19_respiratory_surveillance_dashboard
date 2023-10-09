3b344e812177a4452db2e3719e0e5384b328f3f8
iainma09 <iain.mackinnon2@phs.scot>
2023-10-09 14:26:05 +0100

    creating a method to save out open data files to an archive. still to be completeted

--- Dashboard Data Transfer/dashboard_data_transfer.R
+++ Dashboard Data Transfer/dashboard_data_transfer.R
@@ -117,21 +117,48 @@ source("Transfer Scripts/transfer_respiratory.R")
 
 #(glue(output_folder, "Ethnicity.csv"))
 od_date <- floor_date(today(), "week", 1) + 1
+od_archive_date <-od_date-7
 od_sunday<- floor_date(today(), "week", 1) -1
 od_folder<- "/conf/C19_Test_and_Protect/Test & Protect - Warehouse/Weekly Covid Dashboard/Output/od_outputs/"
+od_archive_folder<- glue("/conf/C19_Test_and_Protect/Test & Protect - Warehouse/Weekly Covid Dashboard/Output/od_outputs/archived/{od_archive_date }")
+
+
+# Set the source directory where your files are located
+
 od_report_date <- format(od_date, "%Y%m%d")
+#od_archive_folder <- glue("{od_folder}/archived/{od_report_date}")
 
 
-# archive location
-# current data sets saved to this location and over written at start of the next week's process 
-# append new row to date report
- # date_previous_report_digit <- gsub("-","",date_previous_report)
- # date_previous_report_digit_plus1 <- gsub("-","",date_previous_report+1) # for folder date - added 05/07/2023
- # date_report_filepath <- glue("{output_filepath}/Archive/{date_previous_report_digit_plus1}/ReportedDate_Scot_Trends_{date_previous_report_digit}.csv")
- # date_report_file <- read_csv(date_report_filepath)
- # date_report_file_updated <- date_report_file %>% add_row (new_row) 
- # 
- # write_csv(date_report_file_updated, glue("{output_filepath}/{resource19}_{date_wednesday}.csv"), na = "")
+# Create the new destination directory if it doesn't exist
+if (!dir.exists(od_archive_folder)) {
+  dir.create(od_archive_folder)
+}
+# Set the source directory where your files are located
+#source_dir <- "/path/to/source/directory"
+
+# Create a vector of file names you want to move
+file_names_to_move <- c(glue("cumulative_testcases_HB2019_{od_report_date}.csv", 
+                         glue("cumulative_testcases_CA2019_{od_report_date}.csv")))
+
+# Set the name of the new destination directory
+#destination_dir <- "/path/to/new/destination/directory"
+
+
+
+# Loop through the file names and move them to the new destination directory
+for (file_name in file_names_to_move) {
+  source_path <- file.path(od_folder, file_name)
+  destination_path <- file.path(od_archive_folder, file_name)
+  
+  # Move the file to the new destination directory
+  if (file.rename(od_folder, od_archive_folder)) {
+    cat("Moved", file_name, "to", destination_dir, "\n")
+  } else {
+    cat("Failed to move", file_name, "\n")
+  }
+}
+
+
 
 # Geography
 source("Transfer Scripts/transfer_geography_open_data.R")
