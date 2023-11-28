3f70b51b63584f8560e18e953cb1b4dc8a7008f1
calumm07 <calum.massie2@phs.scot>
2023-11-28 10:56:52 +0000

    adding new note to mem charts

--- shiny_app/indicators/respiratory_mem/parainfluenza/parainfluenza_mem_server.R
+++ shiny_app/indicators/respiratory_mem/parainfluenza/parainfluenza_mem_server.R
@@ -3,39 +3,39 @@ metadataButtonServer(id="respiratory_parainfluenza_mem",
                      parent = session)
 
 # Low threshold
-parainfluenza_low_threshold <- Respiratory_Pathogens_MEM_Scot %>% 
-  filter(Pathogen == "Parainfluenza Virus") %>% 
-  select(LowThreshold) %>% 
-  distinct() %>% 
+parainfluenza_low_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == "Parainfluenza Virus") %>%
+  select(LowThreshold) %>%
+  distinct() %>%
   .$LowThreshold %>%
   round_half_up(2)
 
 # Moderate threshold
-parainfluenza_moderate_threshold <- Respiratory_Pathogens_MEM_Scot %>% 
-  filter(Pathogen == "Parainfluenza Virus") %>% 
-  select(MediumThreshold) %>% 
-  distinct() %>% 
+parainfluenza_moderate_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == "Parainfluenza Virus") %>%
+  select(MediumThreshold) %>%
+  distinct() %>%
   .$MediumThreshold %>%
   round_half_up(2)
 
 # High threshold
-parainfluenza_high_threshold <- Respiratory_Pathogens_MEM_Scot %>% 
-  filter(Pathogen == "Parainfluenza Virus") %>% 
-  select(HighThreshold) %>% 
-  distinct() %>% 
+parainfluenza_high_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == "Parainfluenza Virus") %>%
+  select(HighThreshold) %>%
+  distinct() %>%
   .$HighThreshold %>%
   round_half_up(2)
 
 # Extraordinary
-parainfluenza_extraordinary_threshold <- Respiratory_Pathogens_MEM_Scot %>% 
-  filter(Pathogen == "Parainfluenza Virus") %>% 
-  select(ExtraordinaryThreshold) %>% 
-  distinct() %>% 
+parainfluenza_extraordinary_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == "Parainfluenza Virus") %>%
+  select(ExtraordinaryThreshold) %>%
+  distinct() %>%
   .$ExtraordinaryThreshold %>%
   round_half_up(2)
 
 # # Get seasons used in line chart
-# seasons_1 <- Respiratory_Pathogens_MEM_Scot %>% 
+# seasons_1 <- Respiratory_Pathogens_MEM_Scot %>%
 #   filter(Pathogen == "Parainfluenza Virus") %>%
 #   select(Season) %>%
 #   arrange(Season) %>%
@@ -51,8 +51,8 @@ parainfluenza_extraordinary_threshold <- Respiratory_Pathogens_MEM_Scot %>%
 # seasons <- seasons$Season
 
 # Get seasons used in line chart
-seasons <- Respiratory_Pathogens_MEM_Scot %>% 
-  filter(Pathogen == "Parainfluenza Virus") %>% 
+seasons <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == "Parainfluenza Virus") %>%
   select(Season) %>%
   arrange(Season) %>%
   distinct() %>%
@@ -64,7 +64,7 @@ seasons <- seasons$Season
 altTextServer("parainfluenza_mem_modal",
               title = "Parainfluenza incidence rate per 100,000 population",
               content = tags$ul(tags$li("This is a plot showing the rate of parainfluenza infection per 100,000 population in Scotland."),
-                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ", 
+                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ",
                                         "Week 40 is typically the start of October and when the winter respiratory season starts."),
                                 tags$li("The y axis shows the rate of parainfluenza infection per 100,000 population."),
                                 tags$li(glue("There is a trace for each of the following seasons: ", seasons[1], ", ",
@@ -72,38 +72,47 @@ altTextServer("parainfluenza_mem_modal",
                                              seasons[6], ".")),
                                 tags$li(glue("Activity levels for parainfluenza based on MEM thresholds are represented by different coloured panels on the plot. ",
                                              "The activity levels and MEM thresholds for parainfluenza are: ",
-                                             "Baseline (< ", parainfluenza_low_threshold, "), ", 
+                                             "Baseline (< ", parainfluenza_low_threshold, "), ",
                                              "Low (", parainfluenza_low_threshold, "-", parainfluenza_moderate_threshold-0.01, "), ",
                                              "Moderate (", parainfluenza_moderate_threshold, "-", parainfluenza_high_threshold-0.01, "), ",
                                              "High (", parainfluenza_high_threshold, "-", parainfluenza_extraordinary_threshold-0.01, "), and ",
-                                             "Extraordinary (>= ", parainfluenza_extraordinary_threshold, ")."))))
+                                             "Extraordinary (>= ", parainfluenza_extraordinary_threshold, ").")),
+                                tags$li("By November 2023, all Community Acute Respiratory Infection (CARI) data were removed from the",
+                                        "overall number of laboratory-confirmed episodes. Changes to activity level thresholds for other",
+                                        "respiratory pathogens were minimal. Influenza activity level thresholds were not affected by this exclusion.")))
 
 altTextServer("parainfluenza_mem_hb_modal",
               title = "Parainfluenza incidence rate per 100,000 population by NHS Health Board",
               content = tags$ul(tags$li(glue("This is a plot showing the rate of parainfluenza infection per 100,000 population by NHS Health Board for seasons ",
                                              seasons[5], " and ", seasons[6], ".")),
-                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ", 
+                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ",
                                         "Week 40 is typically the start of October and when the winter respiratory season starts."),
                                 tags$li("The y axis shows the NHS Health Board."),
                                 tags$li("Each cell is coloured according to the activity level: Baseline, Low, Moderate, High, or Extraordinary."),
                                 tags$li("Caution should be taken when interpreting the activity levels (and MEM thresholds) for smaller NHS Health Boards. ",
                                         "The swab positivity rate shows greater fluctuation as a result of the lower number of samples taken relative ",
                                         "to the population size; this has the effect of generating small or large incidence rates compared to NHS Health Boards ",
-                                        "with larger populations.")))
+                                        "with larger populations."),
+                                tags$li("By November 2023, all Community Acute Respiratory Infection (CARI) data were removed from the",
+                                        "overall number of laboratory-confirmed episodes. Changes to activity level thresholds for other",
+                                        "respiratory pathogens were minimal. Influenza activity level thresholds were not affected by this exclusion.")))
 
 
 altTextServer("parainfluenza_mem_age_modal",
               title = "Parainfluenza incidence rate per 100,000 population by age group",
               content = tags$ul(tags$li(glue("This is a plot showing the rate of parainfluenza infection per 100,000 population by age group for seasons ",
                                              seasons[5], " and ", seasons[6], ".")),
-                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ", 
+                                tags$li("The x axis shows the ISO week of sample, from week 40 to week 39. ",
                                         "Week 40 is typically the start of October and when the winter respiratory season starts."),
                                 tags$li("The y axis shows the age group."),
                                 tags$li("Each cell is coloured according to the activity level: Baseline, Low, Moderate, High, or Extraordinary."),
                                 tags$li("Caution should be taken when interpreting the activity levels (and MEM thresholds) for smaller age groups. ",
                                         "The swab positivity rate shows greater fluctuation as a result of the lower number of samples taken relative ",
                                         "to the population size; this has the effect of generating small or large incidence rates compared to age groups ",
-                                        "with larger populations.")))
+                                        "with larger populations."),
+                                tags$li("By November 2023, all Community Acute Respiratory Infection (CARI) data were removed from the",
+                                        "overall number of laboratory-confirmed episodes. Changes to activity level thresholds for other",
+                                        "respiratory pathogens were minimal. Influenza activity level thresholds were not affected by this exclusion.")))
 
 
 # Parainfluenza MEM table
@@ -167,7 +176,7 @@ output$parainfluenza_mem_plot <- renderPlotly({
   Respiratory_Pathogens_MEM_Scot %>%
     filter(Pathogen == "Parainfluenza Virus") %>%
     create_mem_linechart()
-  
+
 })
 
 # Parainfluenza MEM by HB plot
@@ -176,7 +185,7 @@ output$parainfluenza_mem_hb_plot <- renderPlotly({
     filter(Pathogen == "Parainfluenza Virus") %>%
     mutate(ActivityLevel = factor(ActivityLevel, levels = activity_levels)) %>%
     create_mem_heatmap(breakdown_variable = "HBCode")
-  
+
 })
 
 
@@ -186,7 +195,7 @@ output$parainfluenza_mem_age_plot <- renderPlotly({
     filter(Pathogen == "Parainfluenza Virus") %>%
     mutate(ActivityLevel = factor(ActivityLevel, levels = activity_levels)) %>%
     create_mem_heatmap(breakdown_variable = "AgeGroup")
-  
+
 })
 
 
