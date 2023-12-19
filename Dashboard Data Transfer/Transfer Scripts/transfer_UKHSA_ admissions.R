ba36995d8704c7584df81750504ff46ce286154f
iainma09 <iain.mackinnon2@phs.scot>
2023-12-19 09:21:21 +0000

    add row.names = FALSE to write.csv command

--- Dashboard Data Transfer/Transfer Scripts/transfer_UKHSA_ admissions.R
+++ Dashboard Data Transfer/Transfer Scripts/transfer_UKHSA_ admissions.R
@@ -56,7 +56,8 @@ ukhsa_adm_agegroup_pivot<-ukhsa_adm__agegroup %>%
   select(AdmissionDate,'0-5','6-17','18-24','25-34','35-44','45-54',
          '55-64','65-69', '70-74', '75-84','85+', 'All ages')
 
-  write.csv(ukhsa_adm_agegroup_pivot, glue(ukhsa_adm, "daily_adms.csv"), na="")
+  write.csv(ukhsa_adm_agegroup_pivot, glue(ukhsa_adm, "daily_adms.csv"), 
+            row.names=FALSE, na="")
   
   rm(i_chiadm, ukhsa_adm__agegroup, ukhsa_adm_all_ages, ukhsa_adm_daily,
      ukhsa_adm_agegroup_pivot)
