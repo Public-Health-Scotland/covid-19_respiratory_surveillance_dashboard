6faab935e8dea9dbe097acdcf24a6da8630ee83a
markha06 <mark.hamilton4@phs.scot>
2024-02-23 11:42:13 +0000

    Small fix to mpn

--- shiny_app/indicators/respiratory_mem/mycoplasma_pneumoniae/mycoplasma_pneumoniae_cari_server.R
+++ shiny_app/indicators/respiratory_mem/mycoplasma_pneumoniae/mycoplasma_pneumoniae_cari_server.R
@@ -5,7 +5,7 @@ metadataButtonServer(id="respiratory_mycoplasma_pneumoniae_cari",
 
 altTextServer("mycoplasma_pneumoniae_cari_modal",
               title = "CARI - Swab positivity for Mycoplasma Pneumoniae",
-              content = tags$ul(tags$li("This is a plot showing the swab positivity rate of Mycoplasma Pneumoniae infection in the Community Acute Respiratory Infection (CARI) surveillance programme."),
+              content = tags$ul(tags$li("This is a plot showing the swab positivity rate of Mycoplasma pneumoniae infection in the Community Acute Respiratory Infection (CARI) surveillance programme."),
                                 tags$li("The x axis is the week ending date, starting 09 October 2022."),
                                 tags$li("The y axis is the swab positivity rate."),
                                 tags$li("The solid purple line is the specified swab positivity rate and the lighter purple area around the line indicates the confidence interval."),
@@ -13,7 +13,7 @@ altTextServer("mycoplasma_pneumoniae_cari_modal",
 
 altTextServer("mycoplasma_pneumoniae_cari_age_modal",
               title = "CARI - Swab positivity for Mycoplasma Pneumoniae by age group",
-              content = tags$ul(tags$li("This is a plot showing the swab positivity rate of Mycoplasma Pneumoniae infection by age group in the Community Acute Respiratory Infection (CARI) surveillance programme."),
+              content = tags$ul(tags$li("This is a plot showing the swab positivity rate of Mycoplasma pneumoniae infection by age group in the Community Acute Respiratory Infection (CARI) surveillance programme."),
                                 tags$li("The x axis is the week ending date, starting 09 October 2022."),
                                 tags$li("The y axis is the swab positivity rate."),
                                 tags$li("The plot contains a trace showing the swab positivity rate for each of the following age groups: 0-4 years, 5-14 years, 15-44 years, 45-64 years, 65-74 years, and 75+ years.")))
