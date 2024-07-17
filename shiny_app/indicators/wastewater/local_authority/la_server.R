e2047fd32484232a6383915a2ecd098f11e3b78e
markha06 <mark.hamilton4@phs.scot>
2024-07-17 08:54:19 +0100

    Adding local authority content

---
+++ shiny_app/indicators/wastewater/local_authority/la_server.R
@@ -0,0 +1,68 @@
+
+metadataButtonServer(id="la_wastewater_metadata",
+                     panel="Wastewater",
+                     parent = session)
+
+output$council_area_plot =  renderPlotly({
+  filtered_data <- COVID_Wastewater_CA_table %>% filter(council_area == input$selected_area)
+  p <- plot_ly() %>%
+    add_trace(
+      data = filtered_data, 
+      x = ~End, 
+      y = ~average, 
+      type = 'scatter', 
+      mode = 'lines+markers',
+      line = list(dash = 'solid', color = "#3F3685"),
+      marker = list(color = "#3F3685"),
+      name = input$selected_area,
+      text = ~paste("Week Ending Date:", End, "<br>Average:", round(average, 2), "<br>Coverage:", round(coverage, 2)),
+      hoverinfo = "text"
+    ) %>%
+    # Add the Scotland data
+    add_trace(
+      data = HB_scotland, 
+      x = ~End, 
+      y = ~average, 
+      type = 'scatter', 
+      mode = 'lines+markers',
+      line = list(dash = 'solid', color = "green"),  # Different style for Scotland
+      marker = list(color = "green"),
+      name = 'Scotland',
+      text = ~paste("Week Ending Date:", End, "<br>Scotland Average:", round(average, 2), "<br>Scotland Coverage:", round(coverage, 2)),
+      hoverinfo = "text"
+    ) %>%
+    layout(title = paste("COVID-19 wastewater viral RNA (Mgc/p/d) for", input$selected_area),
+           xaxis = list(title = "Week Ending Date"),
+           yaxis = list(title = "Wastewater viral RNA (Mgc/p/d)"),
+           paper_bgcolor = phs_colours("phs-liberty-10"),
+           plot_bgcolor = phs_colours("phs-liberty-10")) %>% 
+    
+    config(displaylogo = FALSE, displayModeBar = TRUE,
+           modeBarButtonsToRemove = bttn_remove)
+  
+  
+  p
+  
+  
+})
+
+altTextServer("CA_modal",
+              title = "Weekly trend in wastewater COVID-19",
+              content = tags$ul(tags$li("This is a plot showing the weekly trend in wastewater COVID-19 for the selected Local authority.")
+              ))
+
+COVID_Wastewater_CA_table$average = round(COVID_Wastewater_CA_table$average,2)
+COVID_Wastewater_CA_table$coverage = round(COVID_Wastewater_CA_table$coverage,2)
+
+output$council_area_table <- renderDataTable({
+  filtered <- COVID_Wastewater_CA_table %>%
+    filter(council_area == input$selected_area)
+  filtered[-1] %>% 
+    arrange(desc(End)) %>%
+    dplyr::rename('Week Ending Date' = End) %>% 
+    dplyr::rename('Local Authority' = council_area) %>% 
+    dplyr::rename('Average (Mgc/p/d)' = average) %>%
+    dplyr::rename('Coverage (Mgc/p/d)' = coverage) %>% 
+    make_table(order_by_firstcol = "desc")
+  
+})
---
+++ shiny_app/indicators/wastewater/local_authority/la_ui.R
@@ -0,0 +1,36 @@
+
+tagList(
+  fluidRow(
+    tags$head(
+      tags$style(HTML("
+      .control-label {
+        color: black;
+      }
+    "))),
+    metadataButtonUI("la_wastewater_metadata"),
+    fluidRow(width = 12,
+             tagList(h2("Average trends in COVID-19 wastewater RNA by Local authority"))),
+    linebreaks(1),
+    selectInput("selected_area", "Local Authority of interest:", 
+                choices = sort(unique(COVID_Wastewater_CA_table$council_area)),
+                selected = sort(unique(COVID_Wastewater_CA_table$council_area))[1]),
+    tabBox(width=NULL,
+           type="pills",
+           tabPanel("Plot",
+                    tagList(linebreaks(1),
+                            altTextUI("CA_modal"),
+                            withNavySpinner(plotlyOutput("council_area_plot")),
+                            fluidRow(column(
+                              width=12, linebreaks(5),
+                            )))),
+           tabPanel("Data",
+                    tagList(linebreaks(1),
+                            
+                            withNavySpinner(dataTableOutput("council_area_table"))
+                            
+                    )
+           )
+    )
+    
+  )
+)#taglist
