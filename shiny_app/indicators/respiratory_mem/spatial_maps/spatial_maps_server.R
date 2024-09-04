5106b79ad52a7c6b0e4f080ee70976d6c23ccba9
iainma09 <iain.mackinnon2@phs.scot>
2024-09-04 15:51:10 +0100

    further amendments to the branch

--- shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_server.R
+++ shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_server.R
@@ -1,92 +1,44 @@
-# 
-# metadataButtonServer(id="respiratory_rsv_mem",
-#                      panel="Respiratory infection activity",
-#                      parent = session)
-# 
-# 
-# 
 
-
-##### create a map #############
-
-# server <- function(input, output, session) {
-#   
-#   observe({
-#     # Prepare available dates as character strings
-#     available_dates <- as.character(unique(respiratory_pathogens_MEM_hb$WeekEnding))
-#     
-#     # Pass available dates to the JavaScript function
-#     runjs(sprintf("shinyjs.disableDates(%s);", jsonlite::toJSON(available_dates)))
-#   })
-# }
-server <- function(input, output, session) {
-  
-  observe({
-    # Prepare available dates as character strings
-    available_dates <- as.character(unique(Respiratory_Pathogens_MEM_HB_Two_Seasons$WeekEnding))
-    
-    # Generate JavaScript to disable dates not in the available_dates array
-    js_code <- sprintf("
-      var availableDates = %s;
-      var dateInput = document.querySelector('.shiny-date-input input');
-      
-    flatpickr(dateInput, {
-        enable: availableDates.map(date => new Date(date)),
-        dateFormat: 'Y-m-d'
-      });
-    ", jsonlite::toJSON(available_dates))
-    
-    # Run the JavaScript to apply the filtering
-    runjs(js_code)
-  })
-}
-
-
-mem_map_pathogen_filter <- Respiratory_Pathogens_MEM_HB_Two_Seasons%>%
- # filter(Pathogen == input$map_season_filter)%>% 
-  mutate(Pathogen = factor(Pathogen,
-                           levels = c("Influenza","Respiratory syncytial virus",
-                                      "Adenovirus",
-                                      "Human metapneumovirus",
-                                      "Mycoplasma pneumoniae",
-                                      "Parainfluenza virus",
-                                      "Rhinovirus",
-                                      "Seasonal Coronavirus (non-COVID-19)"  )))
-# 
-# mem_map_season_filter <- Respiratory_Pathogens_MEM_HB_Two_Seasons %>% 
-#   mutate(Season = factor(Season,
-#                            levels = c("2023/2024","2022/2023"  )))
-
-map_iso_week_ending <- reactive({
-  selected_week <- input$ISOWeekSelector
-  map_selected_date <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
-    filter(Pathogen == "Adenovirus" & HBName=="NHS Western Isles") %>% # i.e. 1 x HB and 1 x pathogen
-    filter(ISOWeekSelector== selected_week) %>%
-    select(ISOWeekSelector)
-  # %>%
-  #   mutate(WeekEnding=as.Date(WeekEnding))
-  # map_selected_date$WeekEnding <- format(map_selected_date$WeekEnding, "%d %b %y")
-  map_selected_date
+mem_map_pathogen_filter <-reactive({
+  selected_pathogen == input$map_pathogen_filter 
+  map_selected_pathogen<- Two_Seasons_Pathogens_MEM_HB_Polygons %>% 
+     filter(Pathogen == input$map_pathogen_filter) %>% 
+    select(Pathogen) %>% 
+    mutate(Pathogen = factor(Pathogen,
+                       levels = c("Influenza",
+                                  "Respiratory syncytial virus",
+                                  "Adenovirus",
+                                  "Human metapneumovirus",
+                                  "Mycoplasma pneumoniae",
+                                  "Parainfluenza virus",
+                                  "Rhinovirus",
+                                  "Seasonal Coronavirus (non-COVID-19)"  )))
 })
 
-# Create a date slider reactive expression "adenovirus_map_selected_date"
-# map_week_ending <- reactive({
-#   selected_week <- input$map_week_ending
-#   map_selected_date <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
-#     filter(Pathogen == "Adenovirus" & HBName=="NHS Western Isles") %>% # i.e. 1 x HB and 1 x pathogen
-#     filter(WeekEnding== selected_week) %>%
-#     select(WeekEnding) %>%
-#     mutate(WeekEnding=as.Date(WeekEnding))
-#   map_selected_date$WeekEnding <- format(map_selected_date$WeekEnding, "%d %b %y")
-#   map_selected_date
-# })
 
+mem_map_season_filter <-reactive({
+  selected_season == input$map_season_filter
+  map_selected_season<- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+    filter(Season== input$map_season_filter) %>% 
+    select(Sesaon) %>% 
+    mutate(Season = factor(Season, levels = c("2023/2024","2022/2023"  )))
+}) 
+
+map_iso_week_ending <- reactive({
+  selected_week <- input$map_iso_week_filter
+  map_selected_date <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+   # filter(Pathogen == "Adenovirus" & HBName=="NHS Western Isles") %>% # i.e. 1 x HB and 1 x pathogen
+    filter(ISOWeekSelector== selected_week) %>%
+    select(ISOWeekSelector)
+  
+  # map_selected_date
+})
 
 output$mem_map_two_seasons <- renderLeaflet({
   filtered_data  <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
- #  filter(Season==input$map_season_filter) %>%
+   filter(Season==input$map_season_filter) %>%
    filter(Pathogen == input$map_pathogen_filter) %>%
-    filter(WeekEnding== input$map_week_ending)%>% 
+    filter(latest_isoweek_map== input$map_iso_week_filter)%>% 
    # Check if filtered data is not empty
     #     if (nrow(filtered_data) == 0) {
     #   leaflet() %>%
--- shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_ui.R
+++ shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_ui.R
@@ -1,7 +1,11 @@
 
+latest_isoweek_map <- Respiratory_Pathogens_MEM_HB_Two_Seasons %>%
+  tail(1) %>%
+  .$ISOWeekSelector
+
 
 tagList(
-  useShinyjs(),  # Initialize shinyjs
+ # useShinyjs(),  # Initialize shinyjs
   
   # # Date input for week ending selection
   # fluidRow(
@@ -30,45 +34,31 @@ tagList(
   # #          tagList(h2(textOutput("adenovirus_map_dynamic_header")))),# fluidrow
   # 
   fluidRow( 
-    column(width = 6,# mem pathogen
+    column(width = 4,# mem pathogen
            pickerInput(inputId = "map_pathogen_filter",  
                        label = "Select pathogen",
-                       choices = {Respiratory_Pathogens_MEM_HB_Two_Seasons %>%
+                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
                            .$Pathogen %>%
                            unique()}, selected = "Influenza") ),#column
-    column(width = 6,# mem pathogen
+    column(width =4,     #mem season
+           pickerInput(inputId = "map_season_filter",
+                       label = "Select a respiratory season",
+                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%  
+                           .$Season %>%   
+                           unique()},    selected = "2023/2024")),# column
+    column(width = 4,# mem pathogen
            pickerInput(inputId = "map_iso_week_filter",  
                        label = "Select ISO week",
-                       choices = {Respiratory_Pathogens_MEM_HB_Two_Seasons %>%
+                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
                            .$ISOWeekSelector %>%
-                           unique()}
-                       #, selected = "Influenza"
-                       ) ),
+                           unique()}, 
+                       selected = latest_isoweek_map) ),# column
   ),
-    
-  # column(width =4,     #mem season  
-  #        pickerInput(inputId = "map_season_filter",
-  #                    label = "Select a respiratory season",
-  #                    choices = {Respiratory_Pathogens_MEM_HB_Two_Seasons %>%  .$Season %>%   unique()},
-  #                   selected = "2023/2024")),
-
-  #        dateInput(inputId = "map_week_ending",
-  #                  label = "Select Week Ending Date",
-  #                  value = data_recent_date,
-  #                  format = "yyyy-mm-dd")
-                #   ),
-                   # label ="Select Date",
-                   # value = max( Respiratory_Pathogens_Daily_MEM_HB_Two_Seasons$WeekEnding),
-                   # # min = min(Respiratory_Pathogens_MEM_HB_Two_Seasons$WeekEnding),
-                   # # max =  max(Respiratory_Pathogens_MEM_HB_Two_Seasons$WeekEnding),
-                   # format = "yyyy-mm-dd",
-                   # startview = "month",
-                   # weekstart = 1)),# column
-    fluidRow( width = 12,
+  
+  fluidRow( width = 12,
               box("A dynamic map place holder",   width = 12,
                   leafletOutput("mem_map_two_seasons",
                                 width = "100%",height="750px")),
-    #),# fluid row
     linebreaks(1),
-    )
+    )# fluidrow
 )# tag list
