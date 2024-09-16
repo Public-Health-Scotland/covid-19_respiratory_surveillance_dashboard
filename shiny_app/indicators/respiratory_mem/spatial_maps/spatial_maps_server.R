edde4491042aca04618cfa692f440fe0eea1f379
iainma09 <iain.mackinnon2@phs.scot>
2024-09-16 16:26:07 +0100

    making it consistent with age/sex influenza charts, issues with dates still to be resplved

--- shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_server.R
+++ shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_server.R
@@ -1,44 +1,56 @@
 
-mem_map_pathogen_filter <-reactive({
-  selected_pathogen == input$map_pathogen_filter 
-  map_selected_pathogen<- Two_Seasons_Pathogens_MEM_HB_Polygons %>% 
-     filter(Pathogen == input$map_pathogen_filter) %>% 
-    select(Pathogen) %>% 
-    mutate(Pathogen = factor(Pathogen,
-                       levels = c("Influenza",
-                                  "Respiratory syncytial virus",
-                                  "Adenovirus",
-                                  "Human metapneumovirus",
-                                  "Mycoplasma pneumoniae",
-                                  "Parainfluenza virus",
-                                  "Rhinovirus",
-                                  "Seasonal Coronavirus (non-COVID-19)"  )))
-})
+# mem_map_pathogen_filter <-reactive({
+#   selected_pathogen == input$map_pathogen_filter
+#   map_selected_pathogen<- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+#      filter(Pathogen == input$map_pathogen_filter) %>%
+#     select(Pathogen) %>%
+#     mutate(Pathogen = factor(Pathogen,
+#                        levels = c("Influenza",
+#                                   "Respiratory syncytial virus",
+#                                   "Adenovirus",
+#                                   "Human metapneumovirus",
+#                                   "Mycoplasma pneumoniae",
+#                                   "Parainfluenza virus",
+#                                   "Rhinovirus",
+#                                   "Seasonal Coronavirus (non-COVID-19)"  )))
+# })
+# 
+# 
+# mem_map_season_filter <-reactive({
+#   selected_season == input$map_season_filter
+#   map_selected_season<- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+#     filter(Season== input$map_season_filter) %>%
+#     select(Sesaon) %>%
+#     mutate(Season = factor(Season, levels = c("2023/2024","2022/2023"  )))
+# })
+# 
+# mem_map_date_filter <- reactive({
+#   selected_week <- input$map_date_filter
+#   map_selected_date <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+#    # filter(Pathogen == "Adenovirus" & HBName=="NHS Western Isles") %>% # i.e. 1 x HB and 1 x pathogen
+#     filter(ISOWeekSelector== selected_week) %>%
+#     select(ISOWeekSelector)
 
-
-mem_map_season_filter <-reactive({
-  selected_season == input$map_season_filter
-  map_selected_season<- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
-    filter(Season== input$map_season_filter) %>% 
-    select(Sesaon) %>% 
-    mutate(Season = factor(Season, levels = c("2023/2024","2022/2023"  )))
-}) 
-
-map_iso_week_ending <- reactive({
-  selected_week <- input$map_iso_week_filter
-  map_selected_date <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
-   # filter(Pathogen == "Adenovirus" & HBName=="NHS Western Isles") %>% # i.e. 1 x HB and 1 x pathogen
-    filter(ISOWeekSelector== selected_week) %>%
-    select(ISOWeekSelector)
-  
   # map_selected_date
-})
+# })
 
 output$mem_map_two_seasons <- renderLeaflet({
-  filtered_data  <- Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+  selected_date<-as.Date(input$map_data_filter, format = "%d %b %y") #"%Y %m %d"
+  
+  filtered_data<-Two_Seasons_Pathogens_MEM_HB_Polygons %>%
    filter(Season==input$map_season_filter) %>%
    filter(Pathogen == input$map_pathogen_filter) %>%
-    filter(latest_isoweek_map== input$map_iso_week_filter)%>% 
+    #date = {input$respiratory_date %>% as.Date(format="%d %b %y")}
+  #  filter(WeekEnding== selected_date) #ensure dates in same format
+    
+    
+    # filter(FluOrNonFlu == "flu") %>%
+    # filter_by_sex_age(., season = input$respiratory_season,
+    #                   date = {input$respiratory_date %>% as.Date(format="%d %b %y")},
+    #                   breakdown = input$respiratory_select_age_sex_breakdown) %>%
+    # make_age_sex_plot(., breakdown = input$respiratory_select_age_sex_breakdown)
+    
+
    # Check if filtered data is not empty
     #     if (nrow(filtered_data) == 0) {
     #   leaflet() %>%
--- shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_ui.R
+++ shiny_app/indicators/respiratory_mem/spatial_maps/spatial_maps_ui.R
@@ -33,32 +33,78 @@ tagList(
   #  fluidRow(width = 12,
   # #          tagList(h2(textOutput("adenovirus_map_dynamic_header")))),# fluidrow
   # 
-  fluidRow( 
-    column(width = 4,# mem pathogen
-           pickerInput(inputId = "map_pathogen_filter",  
-                       label = "Select pathogen",
-                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
-                           .$Pathogen %>%
-                           unique()}, selected = "Influenza") ),#column
-    column(width =4,     #mem season
-           pickerInput(inputId = "map_season_filter",
-                       label = "Select a respiratory season",
-                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%  
-                           .$Season %>%   
-                           unique()},    selected = "2023/2024")),# column
-    column(width = 4,# mem pathogen
-           pickerInput(inputId = "map_iso_week_filter",  
-                       label = "Select ISO week",
-                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
-                           .$ISOWeekSelector %>%
-                           unique()}, 
-                       selected = latest_isoweek_map) ),# column
-  ),
+  # fluidRow( 
+  #   column(width = 4,# mem pathogen
+  #          pickerInput(inputId = "map_pathogen_filter",  
+  #                      label = "Select pathogen",
+  #                      choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+  #                          .$Pathogen %>%
+  #                          unique()}, selected = "Influenza") ),#column
+  #   column(width =4,     #mem season
+  #          pickerInput(inputId = "map_season_filter",
+  #                      label = "Select a respiratory season",
+  #                      choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%  
+  #                          .$Season %>%   
+  #                          unique()},    selected = "2023/2024")),# column
+  #   column(width = 4,# mem pathogen
+  #          pickerInput(inputId = "map_iso_week_filter",  
+  #                      label = "Select ISO week",
+  #                      choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+  #                          .$ISOWeekSelector %>%
+  #                          unique()}, 
+  #                      selected = latest_isoweek_map) ),# column
+  # ),
+  # 
+ 
+
+
+  fluidRow(width = 12,
+          # type = "pills",
+           #tabPanel(#"Plot",
+                            tagList(
+                              linebreaks(1),
+                              # adding selection for flu subtype
+                              fluidRow(
+                                column(4, pickerInput("map_pathogen_filter",
+                                                      label="Select a pathogen",
+                                                      choices= {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+                                                          .$Pathogen %>% 
+                                                          unique()}, 
+                                                      selected = "Influenza")),
+                                column(4, pickerInput("map_season_filter",
+                                                      label = "Select a season",
+                                                      choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>%
+                                                          .$Season %>% 
+                                                          unique()},
+                                                      selected = "2023/2024")
+                                ),
+                                # column(4, pickerInput("map_date_filter",
+                                #                       label = "Select a date",
+                                #                       choices = {Two_Seasons_Pathogens_MEM_HB_Polygons %>% filter(Season == "2023/2024") %>%
+                                #                           .$WeekEnding %>% unique() %>% as.Date() %>% format("%d %b %y")},
+                                #                       selected = {Two_Seasons_Pathogens_MEM_HB_Polygons %>% 
+                                #                           filter(Season == "2023/2024") %>%
+                                #                           .$WeekEnding %>% 
+                                #                           max() %>%
+                                #                           as.Date() %>%
+                                #                           format("%d %b %y")
+                                #                         })
+                               # ),
+                                
+                              ),# fluid row
+                             # altTextUI("influenza_age_sex"),
+                             # withNavySpinner(leafletOutput("mem_map_two_seasons"))
+                            ) # tagList
+                   ), # fluidRow
   
-  fluidRow( width = 12,
+   
+#   
+   fluidRow( width = 12,
               box("A dynamic map place holder",   width = 12,
+                  withNavySpinner(
                   leafletOutput("mem_map_two_seasons",
-                                width = "100%",height="750px")),
+                                width = "100%",height="750px")
+                  )) ,
     linebreaks(1),
     )# fluidrow
-)# tag list
+ )# tag list
