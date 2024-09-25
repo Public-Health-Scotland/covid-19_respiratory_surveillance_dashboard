843da9f64b1d15a35f2629783ec2b7ec05764b66
iainma09 <iain.mackinnon2@phs.scot>
2024-09-25 09:31:45 +0100

    starting to think about a line chart that reacts to the map click.

--- shiny_app/indicators/spatial_maps/spatial_maps_server.R
+++ shiny_app/indicators/spatial_maps/spatial_maps_server.R
@@ -1,3 +1,37 @@
+# Low threshold
+map_low_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == input$map_pathogen_filter) %>%
+  select(LowThreshold) %>%
+  distinct() %>%
+  .$LowThreshold %>%
+  round_half_up(2)
+
+# Moderate threshold
+map_moderate_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen==  input$map_pathogen_filter) %>%
+  select(MediumThreshold) %>%
+  distinct() %>%
+  .$MediumThreshold %>%
+  round_half_up(2)
+
+# High threshold
+map_high_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == input$map_pathogen_filter) %>%
+  select(HighThreshold) %>%
+  distinct() %>%
+  .$HighThreshold %>%
+  round_half_up(2)
+
+# Extraordinary
+map_extraordinary_threshold <- Respiratory_Pathogens_MEM_Scot %>%
+  filter(Pathogen == input$map_pathogen_filter) %>%
+  select(ExtraordinaryThreshold) %>%
+  distinct() %>%
+  .$ExtraordinaryThreshold %>%
+  round_half_up(2)
+
+
+
 
 
 output$mem_map_two_seasons <- renderLeaflet({
@@ -25,4 +59,23 @@ observeEvent(input$map_season_filter, {
                         max() %>%
                         as.Date() %>%
                         format("%d %b %y")})
+})
+
+# update the varible rv_fuel_poverty
+observeEvent(input$employees_living_wage_cw_map_shape_click,{
+  rv_employees_living_wage_cw(input$employees_living_wage_cw_map_shape_click$id)
+  
+  updateSelectizeInput(session, "employees_living_wage_cw_LA_input",
+                       #label = paste("Select input label", length(x)),
+                       choices = input$employees_living_wage_cw_map_shape_click$id)
+})
+
+
+
+# Influenza MEM plot
+output$map_mem_plot <- renderPlotly({
+  Respiratory_Pathogens_MEM_HB_Two_Seasons %>%
+    filter(Pathogen == input$map_pathogen_filter) %>%
+    create_mem_linechart()
+  
 })
